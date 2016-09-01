44cb22db8768179a11a05be8bfa99db1603a1789
shale <shale99@users.noreply.github.com>
2016-09-01 15:24:11 +0300

    refactor

--- domain-models-runtime/src/main/java/com/folio/rest/resource/interfaces/ShutdownAPI.java
+++ domain-models-runtime/src/main/java/com/folio/rest/resource/interfaces/ShutdownAPI.java
@@ -1,14 +1,10 @@
-package com.sling.rest.resource.interfaces;
+package com.folio.rest.resource.interfaces;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 
-/**
- * @author shale
- *
- */
 public interface ShutdownAPI {
 
   public void shutdown(Vertx vertx, Context context, Handler<AsyncResult<Void>> handler);
