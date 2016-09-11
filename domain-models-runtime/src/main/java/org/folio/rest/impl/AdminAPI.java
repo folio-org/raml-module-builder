c897665bee0ad8073816ad9ab6c4cf17d6fff85d
zeevs <shalev.zeev@gmail.com>
2016-09-11 15:24:23 +0300

    add raml exposing admin interface - allow log level updates

---
+++ domain-models-runtime/src/main/java/org/folio/rest/impl/AdminAPI.java
@@ -0,0 +1,82 @@
+/**
+ * AdminAPI
+ * 
+ * Sep 11, 2016
+ *
+ * Apache License Version 2.0
+ */
+package org.folio.rest.impl;
+
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.logging.Logger;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+
+import org.folio.rest.jaxrs.resource.AdminResource;
+import org.folio.rest.jaxrs.resource.BooksResource.GetBooksResponse;
+import org.folio.rest.tools.utils.LogUtil;
+import org.folio.rest.tools.utils.OutStream;
+
+/**
+ * @author shale
+ *
+ */
+public class AdminAPI implements AdminResource {
+
+  private static final io.vertx.core.logging.Logger log = LoggerFactory.getLogger("AdminAPI");
+
+  
+  @Override
+  public void putAdminLoglevel(Level level, String javaPackage, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext)
+      throws Exception {
+    
+    try {
+      JsonObject updatedLoggers = LogUtil.updateLogConfiguration(javaPackage, level2level(level));
+      OutStream os = new OutStream();
+      os.setData(updatedLoggers);
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withJsonOK(os)));
+    } catch (Exception e) {
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withPlainInternalServerError(
+        "ERROR" + e.getMessage())));
+      log.error(e.getMessage(), e);
+    }
+
+/*    HashSet<Logger> updatedLoggers = LogUtil.updateLogConfiguration(javaPackage, level2level(level));
+    final StringBuffer sb = new StringBuffer();
+    updatedLoggers.forEach( l -> {
+      sb.append("[").append(l.getLevel().getName()).append(":").append(l.getName()).append("]\n");
+    });
+    asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetAdminLoglevelResponse.withPlainOK(sb.toString())));
+    */
+  }
+  
+  @Override
+  public void getAdminLoglevel(Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+
+    try {
+      JsonObject loggers = LogUtil.getLogConfiguration();
+      OutStream os = new OutStream();
+      os.setData(loggers);
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withJsonOK(os)));
+    } catch (Exception e) {
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withPlainInternalServerError(
+        "ERROR" + e.getMessage())));
+      log.error(e.getMessage(), e);
+    }
+  }
+
+  private java.util.logging.Level level2level(Level level){
+    return java.util.logging.Level.parse(level.name());
+  }
+
+
+  
+  
+}
--- domain-models-runtime/src/main/java/org/folio/rest/tools/utils/LogUtil.java
+++ domain-models-runtime/src/main/java/org/folio/rest/tools/utils/LogUtil.java
@@ -1,7 +1,10 @@
 package org.folio.rest.tools.utils;
 
+import java.util.Enumeration;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
@@ -18,14 +21,63 @@ public class LogUtil {
 
     log.info(message1);
   }
+  
+  public static void formatStatsLogMessage(String clientIP, String httpMethod, String httpVersion, int ResponseCode, long responseTime,
+      long responseSize, String url, String queryParams, String message, String body) {
+
+    String message1 = new StringBuilder(clientIP).append(" ").append(httpMethod).append(" ").append(url).append(" ").append(queryParams)
+        .append(" ").append(httpVersion).append(" ").append(ResponseCode).append(" ").append(responseSize).append(" ").append(responseTime)
+        .append(" ").append(message).append(" ").append(body).toString();
+
+    log.info(message1);
+  }
+  
   public static void formatLogMessage(String clazz, String funtion, String message) {
     log.info(new StringBuilder(clazz).append(" ").append(funtion).append(" ").append(message));
   }
   public static void formatErrorLogMessage(String clazz, String funtion, String message) {
     log.error(new StringBuilder(clazz).append(" ").append(funtion).append(" ").append(message));
   }
+  
   public static void closeLogger() {
     LoggerFactory.removeLogger("LogUtil");
-}
+  }
+  
+  public static JsonObject updateLogConfiguration(String packageName, Level level){
+    
+    JsonObject updatedLoggers = new JsonObject();
+    
+    LogManager manager = LogManager.getLogManager();
+    Enumeration<String> loggers = manager.getLoggerNames();
+    while (loggers.hasMoreElements()) {
+      String log = (String) loggers.nextElement();
+      if(log != null && packageName != null && (log.startsWith(packageName) || "*".equals(packageName)) ){
+        java.util.logging.Logger logger = manager.getLogger(log);
+        if(logger != null){
+          logger.setLevel(level);
+          updatedLoggers.put(logger.getName(), logger.getLevel().getName());        
+        }
+      }
+    }
+    return updatedLoggers;
+  }
+  
+  public static JsonObject getLogConfiguration(){
+    
+    JsonObject loggers = new JsonObject();
+    
+    LogManager manager = LogManager.getLogManager();
+    Enumeration<String> loggerNames = manager.getLoggerNames();
+    while (loggerNames.hasMoreElements()) {
+      String log = (String) loggerNames.nextElement();
+      if(log != null){
+        java.util.logging.Logger logger = manager.getLogger(log);
+        if(logger != null && logger.getLevel() != null && logger.getName() != null){
+          loggers.put(logger.getName(), logger.getLevel().getName());
+        }
+      }
+    }
+    return loggers;
+  }
   
 }
