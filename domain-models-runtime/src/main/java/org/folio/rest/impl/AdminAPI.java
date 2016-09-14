8fad63789e2eb98bd60c8e1f57ddcf939a9dd412
zeevs <shalev.zeev@gmail.com>
2016-09-14 16:34:50 +0300

    handle bad boundaries and added unit test with good and bad boundaries

--- domain-models-runtime/src/main/java/org/folio/rest/impl/AdminAPI.java
+++ domain-models-runtime/src/main/java/org/folio/rest/impl/AdminAPI.java
@@ -20,31 +20,30 @@ import org.folio.rest.tools.utils.LogUtil;
 import org.folio.rest.tools.utils.NetworkUtils;
 import org.folio.rest.tools.utils.OutStream;
 
-
 public class AdminAPI implements AdminResource {
 
-  private static final io.vertx.core.logging.Logger log = LoggerFactory.getLogger(AdminAPI.class);
+  private static final io.vertx.core.logging.Logger log              = LoggerFactory.getLogger(AdminAPI.class);
 
-  private static final String DEFAULT_TEMP_DIR = System.getProperty("java.io.tmpdir");
-  
+  private static final String                       DEFAULT_TEMP_DIR = System.getProperty("java.io.tmpdir");
+
   @Validate
   @Override
   public void putAdminLoglevel(Level level, String javaPackage, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext)
       throws Exception {
-    
+
     try {
       JsonObject updatedLoggers = LogUtil.updateLogConfiguration(javaPackage, level2level(level));
       OutStream os = new OutStream();
       os.setData(updatedLoggers);
       asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withJsonOK(os)));
     } catch (Exception e) {
-      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withPlainInternalServerError(
-        "ERROR" + e.getMessage())));
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withPlainInternalServerError("ERROR"
+          + e.getMessage())));
       log.error(e.getMessage(), e);
     }
 
   }
-  
+
   @Override
   public void getAdminLoglevel(Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
 
@@ -54,51 +53,54 @@ public class AdminAPI implements AdminResource {
       os.setData(loggers);
       asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withJsonOK(os)));
     } catch (Exception e) {
-      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withPlainInternalServerError(
-        "ERROR" + e.getMessage())));
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutAdminLoglevelResponse.withPlainInternalServerError("ERROR"
+          + e.getMessage())));
       log.error(e.getMessage(), e);
     }
   }
 
-  private java.util.logging.Level level2level(Level level){
+  private java.util.logging.Level level2level(Level level) {
     return java.util.logging.Level.parse(level.name());
   }
 
   @Override
   public void postAdminUpload(MimeMultipart entity, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext)
       throws Exception {
-    final int parts = entity.getCount();
-    final int []i = new int[]{0};
-    FileSystem fs = vertxContext.owner().fileSystem();
-    for (; i[0] < parts; i[0]++) {
-      BodyPart part = entity.getBodyPart(i[0]);
-      Buffer buff = Buffer.buffer(NetworkUtils.object2Bytes(part.getContent()));
-      final String filename = DEFAULT_TEMP_DIR + "/" + System.currentTimeMillis() + "_" + part.getFileName();
-      fs.writeFile(filename, buff,      
-        new Handler<AsyncResult<Void>>() {
-        @Override
-        public void handle(AsyncResult<Void> result) {
-          try {
-            if(result.succeeded()) {
-              log.info("Uploaded file " + filename);
-              if(i[0] == parts){
-                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PostAdminUploadResponse.withNoContent(part.getFileName())));
+
+    if (entity != null) {
+
+      final int parts = entity.getCount();
+      final int[] i = new int[] { 0 };
+      FileSystem fs = vertxContext.owner().fileSystem();
+      for (; i[0] < parts; i[0]++) {
+        BodyPart part = entity.getBodyPart(i[0]);
+        Buffer buff = Buffer.buffer(NetworkUtils.object2Bytes(part.getContent()));
+        final String filename = DEFAULT_TEMP_DIR + "/" + System.currentTimeMillis() + "_" + part.getFileName();
+        fs.writeFile(filename, buff, new Handler<AsyncResult<Void>>() {
+          @Override
+          public void handle(AsyncResult<Void> result) {
+            try {
+              if (result.succeeded()) {
+                log.info("Uploaded file " + filename);
+                if (i[0] == parts) {
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PostAdminUploadResponse.withNoContent(part.getFileName())));
+                }
+              } else {
+                log.error("Failed uploading file " + filename + ", error: " + result.cause().getMessage(), result.cause());
+                if (i[0] == parts) {
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PostAdminUploadResponse.withPlainInternalServerError("ERROR uploading file")));
+                }
               }
-            } else {
-              log.error("Failed uploading file " + filename
-                + ", error: " + result.cause().getMessage(), result.cause());
-              if(i[0] == parts){
-                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PostAdminUploadResponse.withPlainInternalServerError(
-                  "ERROR uploading file")));
-              }
+            } catch (MessagingException e) {
+              e.printStackTrace();
             }
-          } catch (MessagingException e) {
-            e.printStackTrace();
           }
-        }
-      });
-    }    
+        });
+      }
+    } else {
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PostAdminUploadResponse.withPlainInternalServerError("ERROR uploading file, request was not valid - verify boundaries are identical")));
+    }
+
   }
 
-  
 }
