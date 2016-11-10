2ac2b252139a97ad8755001464a7cecfd08712b3
Julian Ladisch <julianladisch@users.noreply.github.com>
2016-11-10 19:31:43 +0100

    fix .properties files
    maven: copy .properties using ISO-8859-1
    use standard file name schema (APIMessages_en.properties) to help translation toolchain and trigger IDEs to encode them correctly
    accept old file name schema during transition time
    add German translation

--- domain-models-runtime/src/test/java/org/folio/rest/tools/MessagesTest.java
+++ domain-models-runtime/src/test/java/org/folio/rest/tools/MessagesTest.java
@@ -1,15 +1,26 @@
 package org.folio.rest.tools;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 import org.folio.rest.tools.messages.*;
 import org.junit.Test;
 
 public class MessagesTest {
+  private static String __(String language, MessageEnum consts, Object... messageArguments) {
+    return Messages.getInstance().getMessage(language, consts, messageArguments);
+  }
+
   @Test
   public void getMessage() {
-    Messages m = Messages.getInstance();
-    assertEquals("Content-type header must be a but it is \"b\"", m.getMessage("en", "10006", "a", "b"));
-    assertEquals("Content-type header must be a but it is \"b\"", m.getMessage("__", "10006", "a", "b"));
-    assertEquals("Content-type header must be a but it is \"b\"", m.getMessage(null, "10006", "a", "b"));
+    MessageConsts c = MessageConsts.ContentTypeError;
+    assertEquals("Content-type header must be a but it is \"b\"",      __("en", c, "a", "b"));
+    assertEquals("Content-type header must be a but it is \"b\"",      __("__", c, "a", "b"));
+    assertEquals("Content-type header must be a but it is \"b\"",      __(null, c, "a", "b"));
+    assertEquals("Content-type Header muss a sein, aber er ist \"b\"", __("de", c, "a", "b"));
+  }
+
+  @Test
+  public void umlaut() {
+    assertEquals("Operation wird nicht unterstützt", __("de", MessageConsts.OperationNotSupported));
   }
 }
