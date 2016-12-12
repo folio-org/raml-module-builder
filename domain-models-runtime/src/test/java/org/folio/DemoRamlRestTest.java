e6651b0cdf9df33b2ebc0b52dd1b804973598561
shale <shale99@users.noreply.github.com>
2016-12-12 12:31:41 +0200

    clean the test

--- domain-models-runtime/src/test/java/org/folio/DemoRamlRestTest.java
+++ domain-models-runtime/src/test/java/org/folio/DemoRamlRestTest.java
@@ -21,11 +21,17 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import javax.mail.BodyPart;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMultipart;
+
 import org.apache.commons.io.IOUtils;
 import org.folio.rest.RestVerticle;
+import org.folio.rest.client.AdminClient;
 import org.folio.rest.jaxrs.model.Book;
 import org.folio.rest.jaxrs.model.Data;
 import org.folio.rest.jaxrs.model.Datetime;
+import org.folio.rest.jaxrs.resource.AdminResource.PersistMethod;
 import org.folio.rest.tools.utils.NetworkUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -51,16 +57,10 @@ public class DemoRamlRestTest {
     port = NetworkUtils.nextFreePort();
 
     try {
-      startEmbeddedMongo();
       deployRestVerticle(context);
     } catch (Exception e) {
       context.fail(e);
     }
-  }
-
-  private static void startEmbeddedMongo() throws Exception {
-/*    MongoCRUD.setIsEmbedded(true);
-    MongoCRUD.getInstance(vertx).startEmbeddedMongo();*/
   }
 
   private static void deployRestVerticle(TestContext context) {
@@ -112,69 +112,19 @@ public class DemoRamlRestTest {
     checkURLs(context, "http://localhost:" + port + "/books", 400);
     checkURLs(context, "http://localhost:" + port + "/x/books", 400); // should be 404
     checkURLs(context, "http://localhost:" + port + "/admin/memory?history=true", 200, "text/html");
-
-    //update periodic handler (MongoStatsPrinter) with which collection to print stats for and at which interval
-    //postData(context, "http://localhost:" + port + "/admin/collstats", Buffer.buffer("{\"books\": 30}"), 200, 0, null);
+    //checkURLs(context, "http://localhost:" + port + "/admin/postgres_active_sessions?dbname=postgres", 200, "application/json");
 
     //check File Uploads
     postData(context, "http://localhost:" + port + "/admin/uploadmultipart", getBody("uploadtest.json", true), 200, 1, null);
     postData(context, "http://localhost:" + port + "/admin/uploadmultipart?file_name=test.json", getBody("uploadtest.json", true),
       200, 1, null);
 
-    //this will create an entry in the job conf and jobs collections as this is done automatically
-    //when param SAVE_AND_NOTIFY is passed.
-    //postData(context, "http://localhost:" + port +
-    //  "/admin/upload?file_name=test.json&bus_address=a.b.c&persist_method=SAVE_AND_NOTIFY",
-    //  Buffer.buffer(getFile("uploadtest.json")), 204, 1, null);
-
     List<Object> list = getListOfBooks();
-
-/*    //check bulk insert Mongo
-    bulkInsert(context, list);
-
-    //check insert with fail if id exists already Mongo
-    insertUniqueTest(context, list.get(0));*/
-
-    //check save and get of binary Mongo
-    binaryInsert(context);
-
-    //check save and get object with encoded binary base64 field Mongo
-    //calls collection list and collection stats when done
-    base64EncTest(context);
 
     checkURLs(context, "http://localhost:" + port + "/apidocs/index.html", 200); // should be 200
 
     //use generated client
     checkClientCode(context);
-
-    //group by
-/*    groupBy(context, "distinct");
-    groupBy(context, "multi");
-    groupBy(context, "max");
-    groupBy(context, "sum");
-    groupBy(context, "avg");
-    groupBy(context, "all");
-    groupBy(context, "pivot"); //for example: return all titles for each genre
-    groupBy(context, null);
-    groupBy(context, "expression");
-    groupBy(context, "date");
-    groupBy(context, "matching");*/
-
-    //jobsTest(context);
-
-/*    AdminClient ac = new AdminClient("localhost", port, "abc");
-    ac.getAdminJstack( response -> {
-      StringBuffer sb = new StringBuffer();
-      response.exceptionHandler( error -> {
-        System.out.println("ERROR -> " + error.getMessage());
-      });
-      response.handler( data -> {
-        sb.append(data);
-      });
-      response.endHandler(endHandler -> {
-        System.out.println("JSTACK" + sb.toString());
-      });
-    });*/
   }
 
   /**
@@ -182,7 +132,7 @@ public class DemoRamlRestTest {
    *
    */
   private void checkClientCode(TestContext context) {
-/*    Async async = context.async(2);
+    Async async = context.async(2);
     System.out.println("checkClientCode test");
     try {
       MimeMultipart mmp = new MimeMultipart();
@@ -194,7 +144,8 @@ public class DemoRamlRestTest {
       bp2.setFileName("abcd.raml");
       mmp.addBodyPart(bp);
       mmp.addBodyPart(bp2);
-      new AdminClient("localhost", port, "abc", false).postUploadmultipart(PersistMethod.SAVE, null, "abc",
+      AdminClient aClient = new AdminClient("localhost", port, "abc", false);
+      aClient.postUploadmultipart(PersistMethod.SAVE, null, "abc",
         mmp, reply -> {
         if(reply.statusCode() != 200){
           context.fail();
@@ -203,7 +154,7 @@ public class DemoRamlRestTest {
         async.countDown();
       });
 
-      new AdminClient("localhost", port, "abc", false).postUploadbinary(PersistMethod.SAVE, null, "abc",
+      aClient.postUploadbinary(PersistMethod.SAVE, null, "abc",
         getClass().getClassLoader().getResourceAsStream("folio.jpg"), reply -> {
           if(reply.statusCode() != 200){
             context.fail();
@@ -216,7 +167,7 @@ public class DemoRamlRestTest {
       context.fail();
       System.out.println(e.getMessage());
       e.printStackTrace();
-    }*/
+    }
   }
 
   /**
@@ -273,279 +224,6 @@ public class DemoRamlRestTest {
     }
     request.end();
   }
-
-  private void getCollectionStats(TestContext context){
-/*    Async async = context.async();
-    MongoCRUD.getInstance(vertx).getStatsForCollection("books", reply -> {
-      if(reply.succeeded()){
-        //System.out.println(reply.result().encodePrettily());
-        context.assertInRange(7 ,(reply.result().getInteger("count")), 1);
-      }
-      else{
-        context.fail(reply.cause().getMessage());
-      }
-      async.complete();
-    });*/
-  }
-
-  private void getCollectionList(TestContext context){
-/*    Async async = context.async();
-    MongoCRUD.getInstance(vertx).getListOfCollections(reply -> {
-      if(reply.succeeded()){
-        context.assertInRange(2 , reply.result().size(), 1);
-        System.out.println("list of collection success");
-      }
-      else{
-        context.fail(reply.cause().getMessage());
-      }
-      async.complete();
-    });*/
-  }
-
-  private void base64EncTest(TestContext context){
-/*    Book b = createBook();
-    String file = getClass().getClassLoader().getResource("folio.jpg").getFile();
-    Buffer buffer = Buffer.buffer();
-    Async async = context.async();
-    vertx.fileSystem().open(file, new OpenOptions(), ar -> {
-      if (ar.succeeded()) {
-        AsyncFile rs = ar.result();
-        rs.handler( buf -> {
-          buffer.appendBuffer(buf);
-        });
-        rs.exceptionHandler(t -> {
-          context.fail();
-          async.complete();
-        });
-        rs.endHandler(v -> {
-          ar.result().close(ar2 -> {
-            if (ar2.failed()) {
-              context.fail();
-            }
-            else{
-              Async async2 = context.async();
-              b.setImage(Base64.getEncoder().encodeToString(buffer.getBytes()));
-              MongoCRUD client = MongoCRUD.getInstance(vertx);
-              client.save("books", b, reply -> {
-                if(reply.succeeded()){
-                  String id = reply.result();
-                  client.get(Book.class.getName(), "books", id, reply2 -> {
-                    Async async3 = context.async();
-                    if(reply2.succeeded()){
-                      byte[] image = Base64.getDecoder().decode(((Book)reply2.result()).getImage());
-                      if(image.length == 2747){
-                        context.assertTrue(true);
-                        System.out.println("save binary success");
-
-                        //check collection stats Mongo
-                        getCollectionStats(context);
-
-                        //get list of collections Mongo
-                        getCollectionList(context);
-                      }
-                      else{
-                        context.fail("size of file is incorrect");
-                      }
-                      try {
-                        org.apache.commons.io.FileUtils.writeByteArrayToFile(new File("test.jpg"), image);
-                      } catch (Exception e) {
-                        e.printStackTrace();
-                      }
-                    }else{
-                      context.fail(reply2.cause().getMessage());
-                    }
-                    async3.complete();
-                  });
-                }
-                else{
-                  context.fail(reply.cause().getMessage());
-                }
-                async2.complete();
-              });
-            }
-          });
-          async.complete();
-        });
-      }});*/
-
-
-  }
-
-  private void binaryInsert(TestContext context){
-    //check binary save in mongo
-/*    String file = getClass().getClassLoader().getResource("folio.jpg").getFile();
-    Buffer buffer = Buffer.buffer();
-    Async async = context.async();
-    vertx.fileSystem().open(file, new OpenOptions(), ar -> {
-      if (ar.succeeded()) {
-        AsyncFile rs = ar.result();
-        rs.handler( buf -> {
-          buffer.appendBuffer(buf);
-        });
-        rs.exceptionHandler(t -> {
-          context.fail();
-          async.complete();
-        });
-        rs.endHandler(v -> {
-          ar.result().close(ar2 -> {
-            if (ar2.failed()) {
-              context.fail();
-            }
-            else{
-              Async async1 = context.async();
-              MongoCRUD client = MongoCRUD.getInstance(vertx);
-              client.saveBinary("files", buffer, "images", reply -> {
-                if(reply.succeeded()){
-                  //context.assertTrue(true, "Binary image file saved successfully");
-                  Async async2 = context.async();
-                  String id = reply.result();
-                  client.getBinary("files","images", 0, 1, res2 -> {
-                    if(res2.succeeded()) {
-
-                      byte[]image = (byte[])((List<?>)res2.result()).get(0);
-                      if(image.length == 2747){
-                        context.assertTrue(true);
-                        System.out.println("save binary success");
-                      }
-                      else{
-                        context.fail("size of file is incorrect");
-                      }
-                    } else {
-                      context.fail(res2.cause().getMessage());
-                    }
-                    async2.complete();
-                  });
-                }
-                else{
-                  context.fail();
-                }
-                async1.complete();
-              });
-            }
-          });
-          async.complete();
-        });
-      } else {
-        context.fail();
-        async.complete();
-      }
-    });*/
-  }
-
-/*  private void groupBy(TestContext context, String op){
-    //check bulk insert in MONGO
-    Async async = context.async();
-    GroupBy gb = new GroupBy();
-    if(op == null){
-      gb = new GroupBy(null);
-    }
-    if("multi".equals(op) || "all".equals(op)){
-      //group by two fields
-      gb.addGroupByField("data.author").addGroupByField("data.genre");
-    }
-    else if("pivot".equals(op)){
-      //show all titles per genre
-      gb.addGroupByField("data.genre", "titles", "data.title");
-    }
-    else if("date".equals(op)){
-      //must pass a valid mongo date field as the second argument
-      gb.addGroupByField("year","data.datetime", DateEnum.YEAR).
-      addGroupByField("month","data.datetime", DateEnum.DAY_OF_MONTH);
-      gb.addCount();
-    }
-    else if(op != null){
-      //group by author
-      gb.addGroupByField("data.author");
-    }
-    if("expression".equals(op)){
-      String expression = "{ \"$sum\": {\"$multiply\": [ \"$status\", \"$status\" ] }}";
-      gb.addConstraint("alias", new JsonObject(expression));
-    }
-    if("sum".equals(op)  || "all".equals(op) || op == null){
-      //add a count value of items in group
-      gb.addCount();
-    }
-    if("avg".equals(op)  || "all".equals(op) || op == null){
-      gb.addAVGForField("average","status");
-    }
-    if("max".equals(op)  || "all".equals(op) || op == null){
-      gb.addMAXForField("max", "status");
-    }
-    if("matching".equals(op)){
-      //remove random values from object and leave only constant values
-      //so that we have a match
-      Book book = createBook();
-      book.getData().setAuthor(null);
-      book.getData().setDatetime(null);
-      book.getData().setTitle(null);
-      book.setStatus(null);
-
-      MongoCRUD.getInstance(vertx).groupBy("books", gb, book, reply -> {
-        if(reply.succeeded()){
-          System.out.println(op + " group by result: "+reply.result());
-        }
-        else{
-          context.fail();
-          System.out.println(reply.cause().getMessage());
-        }
-        async.complete();
-      });
-    }
-    else{
-      MongoCRUD.getInstance(vertx).groupBy("books", gb, reply -> {
-        if(reply.succeeded()){
-          System.out.println(op + " group by result: "+reply.result());
-        }
-        else{
-          context.fail();
-          System.out.println(reply.cause().getMessage());
-        }
-        async.complete();
-      });
-    }
-  }
-
-  private void bulkInsert(TestContext context, List<Object> list){
-    //check bulk insert in MONGO
-    Async async = context.async();
-    MongoCRUD.getInstance(vertx).bulkInsert("books", list, reply -> {
-      if(reply.succeeded()){
-        context.assertEquals(5, reply.result().getInteger("n"),
-          "bulk insert updated " + reply.result().getInteger("n") + " records instead of 5");
-      }
-      else{
-        context.fail();
-        System.out.println(reply.cause().getMessage());
-      }
-      async.complete();
-    });
-  }
-
-  private void insertUniqueTest(TestContext context, Object book){
-    //insert fail if id exists test MONGO
-    Async async2 = context.async();
-    MongoCRUD.getInstance(vertx).save("books", book, reply -> {
-      if(reply.succeeded()){
-        String id = reply.result();
-        ((Book)book).setId(id);
-        Async async3 = context.async();
-        MongoCRUD.getInstance(vertx).save("books", book, true , reply2 -> {
-          if(reply2.succeeded()){
-            context.fail("save succeeded but should have failed!");
-          }
-          else{
-            context.assertTrue(true);
-          }
-          async3.complete();
-        });
-      }
-      else{
-        context.fail();
-        System.out.println(reply.cause().getMessage());
-      }
-      async2.complete();
-    });
-  }*/
 
   public void checkURLs(TestContext context, String url, int codeExpected) {
     String accept = "application/json";
