5473895ecbbd0374571b62d99b0400b0aac01e2d
shale <shale99@users.noreply.github.com>
2016-12-12 13:05:13 +0200

    connect to requested db for table size

--- domain-models-runtime/src/main/java/org/folio/rest/impl/AdminAPI.java
+++ domain-models-runtime/src/main/java/org/folio/rest/impl/AdminAPI.java
@@ -323,7 +323,7 @@ public class AdminAPI implements AdminResource {
   public void getAdminPostgresTableSize(String dbname, Map<String, String> okapiHeaders,
       Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
 
-    PostgresClient.getInstance(vertxContext.owner(), "postgres").select(
+    PostgresClient.getInstance(vertxContext.owner(), dbname).select(
       "SELECT relname as \"Table\", pg_size_pretty(pg_relation_size(relid)) As \" Table Size\","
       + " pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) as \"Index Size\""
       + " FROM pg_catalog.pg_statio_user_tables ORDER BY pg_total_relation_size(relid) DESC;", reply -> {
