ed1fdf2e1397c71ada66010e7b27ef92476cb8d6
shale <shale99@users.noreply.github.com>
2016-12-15 21:06:12 +0200

    add / drop / exists tenant

--- domain-models-runtime/src/main/java/org/folio/rest/impl/TenantAPI.java
+++ domain-models-runtime/src/main/java/org/folio/rest/impl/TenantAPI.java
@@ -35,7 +35,40 @@ public class TenantAPI implements org.folio.rest.jaxrs.resource.TenantResource {
   public void deleteTenant(Map<String, String> headers,
       Handler<AsyncResult<Response>> handlers, Context context) throws Exception {
 
+    context.runOnContext(v -> {
+      try {
 
+        System.out.println("sending... deleteTenant");
+        String tenantId = TenantTool.calculateTenantId( headers.get(OKAPI_HEADER_TENANT) );
+
+        String sqlFile = IOUtils.toString(
+          TenantAPI.class.getClassLoader().getResourceAsStream("delete_tenant.sql"));
+
+        final String sql2run = sqlFile.replaceAll("myuniversity", tenantId);
+
+        /* connect as user in postgres-conf.json file (super user) - so that all commands will be available */
+        PostgresClient.getInstance(context.owner()).runSQLFile(sql2run, false,
+            reply -> {
+              try {
+                String res = "";
+                if(reply.succeeded()){
+                  res = new JsonArray(reply.result()).encodePrettily();
+                }
+                OutStream os = new OutStream();
+                os.setData(res);
+                handlers.handle(io.vertx.core.Future.succeededFuture(DeleteTenantResponse.withJsonOK(os)));
+              } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                handlers.handle(io.vertx.core.Future.succeededFuture(DeleteTenantResponse
+                  .withPlainInternalServerError(e.getMessage())));
+              }
+            });
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        handlers.handle(io.vertx.core.Future.succeededFuture(DeleteTenantResponse
+          .withPlainInternalServerError(e.getMessage())));
+      }
+    });
 
   }
 
@@ -44,6 +77,40 @@ public class TenantAPI implements org.folio.rest.jaxrs.resource.TenantResource {
   public void getTenant(Map<String, String> headers, Handler<AsyncResult<Response>> handlers,
       Context context) throws Exception {
 
+    context.runOnContext(v -> {
+      try {
+
+        System.out.println("sending... postTenant");
+        String tenantId = TenantTool.calculateTenantId( headers.get(OKAPI_HEADER_TENANT) );
+
+        String sqlFile = IOUtils.toString(
+          TenantAPI.class.getClassLoader().getResourceAsStream("create_tenant.sql"));
+
+        final String sql2run = sqlFile.replaceAll("myuniversity", tenantId);
+
+        /* connect as user in postgres-conf.json file (super user) - so that all commands will be available */
+        PostgresClient.getInstance(context.owner()).select(
+          "SELECT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = '"+tenantId+"');",
+            reply -> {
+              try {
+                String res = "";
+                if(reply.succeeded()){
+                }
+                OutStream os = new OutStream();
+                os.setData(res);
+                //handlers.handle(io.vertx.core.Future.succeededFuture(GetTenantResponse.withJsonOK(os)));
+              } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                handlers.handle(io.vertx.core.Future.succeededFuture(GetTenantResponse
+                  .withPlainInternalServerError(e.getMessage())));
+              }
+            });
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        handlers.handle(io.vertx.core.Future.succeededFuture(GetTenantResponse
+          .withPlainInternalServerError(e.getMessage())));
+      }
+    });
   }
 
   @Validate
