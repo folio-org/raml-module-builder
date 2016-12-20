b02eb2937a964bcb4e5831cf709b2edb7d63dda1
shale <shale99@users.noreply.github.com>
2016-12-20 14:01:56 +0200

    check if tenant exists before attempting to add

--- domain-models-runtime/src/main/java/org/folio/rest/impl/TenantAPI.java
+++ domain-models-runtime/src/main/java/org/folio/rest/impl/TenantAPI.java
@@ -79,6 +79,26 @@ public class TenantAPI implements org.folio.rest.jaxrs.resource.TenantResource {
 
   }
 
+  private void tenantExists(Context context, String tenantId, Handler<AsyncResult<Boolean>> handler){
+    /* connect as user in postgres-conf.json file (super user) - so that all commands will be available */
+    PostgresClient.getInstance(context.owner()).select(
+      "SELECT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = '"+ PostgresClient.convertToPsqlStandard(tenantId) +"');",
+        reply -> {
+          try {
+            if(reply.succeeded()){
+              handler.handle(io.vertx.core.Future.succeededFuture(reply.result().getResults().get(0).getBoolean(0)));
+            }
+            else {
+              log.error(reply.cause().getMessage(), reply.cause());
+              handler.handle(io.vertx.core.Future.failedFuture(reply.cause().getMessage()));
+            }
+          } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            handler.handle(io.vertx.core.Future.failedFuture(e.getMessage()));
+          }
+    });
+  }
+
   @Validate
   @Override
   public void getTenant(Map<String, String> headers, Handler<AsyncResult<Response>> handlers,
@@ -90,26 +110,19 @@ public class TenantAPI implements org.folio.rest.jaxrs.resource.TenantResource {
         System.out.println("sending... postTenant");
         String tenantId = TenantTool.calculateTenantId( headers.get(OKAPI_HEADER_TENANT) );
 
-        String sqlFile = IOUtils.toString(
-          TenantAPI.class.getClassLoader().getResourceAsStream(CREATE_TENANT_TEMPLATE));
-
-        String sql2run = sqlFile.replaceAll(TEMPLATE_PLACEHOLDER, tenantId);
-
-        /* connect as user in postgres-conf.json file (super user) - so that all commands will be available */
-        PostgresClient.getInstance(context.owner()).select(
-          "SELECT EXISTS(SELECT 1 FROM pg_namespace WHERE nspname = '"+tenantId+"');",
-            reply -> {
-              try {
-                String res = "";
-                if(reply.succeeded()){
-                }
-                handlers.handle(io.vertx.core.Future.succeededFuture(GetTenantResponse.withPlainOK("")));
-              } catch (Exception e) {
-                log.error(e.getMessage(), e);
-                handlers.handle(io.vertx.core.Future.succeededFuture(GetTenantResponse
-                  .withPlainInternalServerError(e.getMessage())));
-              }
-            });
+        tenantExists(context, tenantId, res -> {
+          boolean exists = false;
+          if(res.succeeded()){
+            exists = res.result();
+            handlers.handle(io.vertx.core.Future.succeededFuture(GetTenantResponse.withPlainOK(String.valueOf(
+              exists))));
+          }
+          else{
+            log.error(res.cause().getMessage(), res.cause());
+            handlers.handle(io.vertx.core.Future.succeededFuture(GetTenantResponse
+              .withPlainInternalServerError(res.cause().getMessage())));
+          }
+        });
       } catch (Exception e) {
         log.error(e.getMessage(), e);
         handlers.handle(io.vertx.core.Future.succeededFuture(GetTenantResponse
@@ -124,72 +137,95 @@ public class TenantAPI implements org.folio.rest.jaxrs.resource.TenantResource {
       Handler<AsyncResult<Response>> handlers, Context context) throws Exception {
 
     /**
-    * http://host:port/tenant
-    */
+     * http://host:port/tenant
+     */
     context.runOnContext(v -> {
+      System.out.println("sending... postTenant");
+      String tenantId = TenantTool.calculateTenantId(headers.get(OKAPI_HEADER_TENANT));
+
       try {
-
-        System.out.println("sending... postTenant");
-        String tenantId = TenantTool.calculateTenantId( headers.get(OKAPI_HEADER_TENANT) );
-
-        String sqlFile = IOUtils.toString(
-          TenantAPI.class.getClassLoader().getResourceAsStream(CREATE_TENANT_TEMPLATE));
-
-        final String sql2run = sqlFile.replaceAll(TEMPLATE_PLACEHOLDER, tenantId);
-
-        /* is there an audit .sql file to load */
-        InputStream audit = TenantAPI.class.getClassLoader().getResourceAsStream(AUDIT_TENANT_TEMPLATE);
-        StringBuffer auditContent = new StringBuffer();
-        if(audit != null){
-          auditContent.append(IOUtils.toString(audit).replace(TEMPLATE_PLACEHOLDER, tenantId));
-        }
-
-        /* connect as user in postgres-conf.json file (super user) - so that all commands will be available */
-        PostgresClient.getInstance(context.owner()).runSQLFile(sql2run, false,
-            reply -> {
-              try {
-                StringBuffer res = new StringBuffer();
-                if(reply.succeeded()){
-                  res.append(new JsonArray(reply.result()).encodePrettily());
-                  OutStream os = new OutStream();
-
-                  if(audit != null){
-                    PostgresClient.getInstance(context.owner()).runSQLFile(auditContent.toString(), false, reply2 -> {
-                      if(reply2.succeeded()){
-                        String auditRes = new JsonArray(reply2.result()).encodePrettily();
-                        os.setData(res + auditRes);
-                        handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse.withJsonOK(os)));
-                      }
-                      else{
-                        log.error(reply2.cause().getMessage(), reply2.cause());
-                        handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse
-                          .withPlainInternalServerError("Created tenant without auditing: " + reply2.cause().getMessage())));
-                      }
-                    });
-                  }
-                  else{
-                    os.setData(res);
-                    handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse.withJsonOK(os)));
-                  }
+        tenantExists(context, tenantId,
+          h -> {
+            try {
+              boolean exists = false;
+              if(h.succeeded()){
+                exists = h.result();
+                if(exists){
+                  handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse.
+                    withPlainInternalServerError("Tenant already exists: " + tenantId)));
+                  log.error("Tenant already exists: " + tenantId);
+                  return;
                 }
                 else{
-                  log.error(reply.cause().getMessage(), reply.cause());
-                  handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse
-                    .withPlainInternalServerError(reply.cause().getMessage())));
+                  log.info("adding tenant " + tenantId);
                 }
-              } catch (Exception e) {
-                log.error(e.getMessage(), e);
-                handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse
-                  .withPlainInternalServerError(e.getMessage())));
               }
-            });
+              else{
+                handlers.handle(io.vertx.core.Future.failedFuture(h.cause().getMessage()));
+                return;
+              }
+              String sqlFile = IOUtils.toString(TenantAPI.class.getClassLoader().getResourceAsStream(
+                CREATE_TENANT_TEMPLATE));
+
+              final String sql2run = sqlFile.replaceAll(TEMPLATE_PLACEHOLDER, tenantId);
+
+              /* is there an audit .sql file to load */
+              InputStream audit = TenantAPI.class.getClassLoader().getResourceAsStream(
+                AUDIT_TENANT_TEMPLATE);
+              StringBuffer auditContent = new StringBuffer();
+              if (audit != null) {
+                auditContent.append(IOUtils.toString(audit).replace(TEMPLATE_PLACEHOLDER, tenantId));
+              }
+              /* connect as user in postgres-conf.json file (super user) - so that all commands will be available */
+              PostgresClient.getInstance(context.owner()).runSQLFile(sql2run, false,
+                reply -> {
+                  try {
+                    StringBuffer res = new StringBuffer();
+                    if (reply.succeeded()) {
+                      res.append(new JsonArray(reply.result()).encodePrettily());
+                      OutStream os = new OutStream();
+
+                      if (audit != null) {
+                        PostgresClient.getInstance(context.owner()).runSQLFile(
+                          auditContent.toString(),
+                          false,
+                          reply2 -> {
+                            if (reply2.succeeded()) {
+                              String auditRes = new JsonArray(reply2.result()).encodePrettily();
+                              os.setData(res + auditRes);
+                              handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse.withJsonOK(os)));
+                            } else {
+                              log.error(reply2.cause().getMessage(), reply2.cause());
+                              handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse.
+                                withPlainInternalServerError("Created tenant without auditing: "
+                                  + reply2.cause().getMessage())));
+                            }
+                          });
+                      } else {
+                        os.setData(res);
+                        handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse.withJsonOK(os)));
+                      }
+                    } else {
+                      log.error(reply.cause().getMessage(), reply.cause());
+                      handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse.
+                        withPlainInternalServerError(reply.cause().getMessage())));
+                    }
+                  } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                    handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse.
+                      withPlainInternalServerError(e.getMessage())));
+                  }
+                });
+            } catch (Exception e) {
+              log.error(e.getMessage(), e);
+              handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse.
+                withPlainInternalServerError(e.getMessage())));
+            }
+          });
       } catch (Exception e) {
         log.error(e.getMessage(), e);
-        handlers.handle(io.vertx.core.Future.succeededFuture(PostTenantResponse
-          .withPlainInternalServerError(e.getMessage())));
       }
     });
-
   }
 
 }
