4cd0f798214754aa6b92218bfbc9931f29000242
Julian Ladisch <julianladisch@users.noreply.github.com>
2017-02-09 11:56:40 +0100

    DMOD-161: BinaryOutStream unit tests

--- domain-models-runtime/src/main/java/org/folio/rest/tools/utils/BinaryOutStream.java
+++ domain-models-runtime/src/main/java/org/folio/rest/tools/utils/BinaryOutStream.java
@@ -3,19 +3,17 @@ package org.folio.rest.tools.utils;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import javax.ws.rs.WebApplicationException;
-
 /**
- * @author shale
+ * Holds and writes a binary byte [].
  *
+ * write(output) throws a NullPointerException if no data has been set or has been set to null.
  */
 public class BinaryOutStream implements javax.ws.rs.core.StreamingOutput {
 
   private byte[] data;
 
   @Override
-  public void write(OutputStream output) throws IOException, WebApplicationException {
-
+  public void write(OutputStream output) throws IOException {
     output.write(this.data);
     output.flush();
   }
