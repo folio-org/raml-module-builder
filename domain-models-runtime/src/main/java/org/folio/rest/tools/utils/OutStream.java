fe866f2962c0764380ec09e8f2ee77260b3505af
Julian Ladisch <julianladisch@users.noreply.github.com>
2017-02-09 14:38:34 +0100

    DMOD-161: OutStream unit tests, fix UTF_8
    * add unit tests
    * add javadoc
    * fix UTF_8 bug with default charset
    * remove BufferedWriter/OutputStreamWriter, no buffering needed for a single write

--- domain-models-runtime/src/main/java/org/folio/rest/tools/utils/OutStream.java
+++ domain-models-runtime/src/main/java/org/folio/rest/tools/utils/OutStream.java
@@ -1,29 +1,37 @@
 package org.folio.rest.tools.utils;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 
-import javax.ws.rs.WebApplicationException;
-
+/**
+ * Holds and writes an Object whose {@link Object#toString()} is used when invoking {@link #write(OutputStream)}.
+ *
+ * {@link #write(OutputStream)} throws a {@link NullPointerException} if no object has been set or has been set to null.
+ */
 public class OutStream implements javax.ws.rs.core.StreamingOutput {
 
   private Object data;
 
   @Override
-  public void write(OutputStream output) throws IOException, WebApplicationException {
-
-    Writer writer = new BufferedWriter(new OutputStreamWriter(output));
-    writer.write(this.data.toString());
-    writer.flush();
+  public void write(OutputStream output) throws IOException {
+    // use UTF_8, do not use Charset.getDefaultCharset()
+    output.write(data.toString().getBytes(StandardCharsets.UTF_8));
+    output.flush();
   }
 
+  /**
+   * Return the object set by the last invocation of {@link #setData(Object)}.
+   * @return
+   */
   public Object getData() {
     return data;
   }
 
+  /**
+   * Set the object whose toString() will be used by {@link #write(OutputStream)}.
+   * @param data  object to write.
+   */
   public void setData(Object data) {
     this.data = data;
   }
