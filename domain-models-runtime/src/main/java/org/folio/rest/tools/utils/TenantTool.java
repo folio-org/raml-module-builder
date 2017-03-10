358dc6e4530c4c6bfc50715292a341102aaf751e
Julian Ladisch <julianladisch@users.noreply.github.com>
2017-03-10 16:26:34 +0100

    DMOD-164 TenantTool headers

--- domain-models-runtime/src/main/java/org/folio/rest/tools/utils/TenantTool.java
+++ domain-models-runtime/src/main/java/org/folio/rest/tools/utils/TenantTool.java
@@ -1,19 +1,33 @@
 package org.folio.rest.tools.utils;
 
+import java.util.Map;
+
+import org.folio.rest.RestVerticle;
+
 /**
- * @author shale
- *
+ * Utility functions for tenant handling.
  */
 public final class TenantTool {
   private TenantTool() {
     throw new UnsupportedOperationException("Cannot instantiate utility class.");
   }
 
-  public static String calculateTenantId(String tenantId){
+  /**
+   * @param tenantId tenant to calculate the tenantId for
+   * @return the tenantId, this is "folio_shared" if tenantId is null
+   */
+  public static String calculateTenantId(String tenantId) {
     if(tenantId == null){
       return "folio_shared";
     }
     return tenantId;
   }
 
+  /**
+   * @param headers HTTP headers to use, may be empty, but not null
+   * @return the tenantId for the headers, returns the default "folio_shared" if undefined
+   */
+  public static String tenantId(Map<String, String> headers) {
+    return calculateTenantId(headers.get(RestVerticle.OKAPI_HEADER_TENANT));
+  }
 }
