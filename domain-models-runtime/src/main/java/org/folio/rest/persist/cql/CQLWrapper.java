6299ded76fa18d8492189a3145ff5e6fdc33ad94
shale <shale99@users.noreply.github.com>
2017-03-17 08:29:24 +0200

    allow nesting cql wrappers in a cql wrapper

--- domain-models-runtime/src/main/java/org/folio/rest/persist/cql/CQLWrapper.java
+++ domain-models-runtime/src/main/java/org/folio/rest/persist/cql/CQLWrapper.java
@@ -1,5 +1,8 @@
 package org.folio.rest.persist.cql;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.folio.rest.persist.Criteria.Limit;
 import org.folio.rest.persist.Criteria.Offset;
 import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
@@ -12,6 +15,8 @@ public class CQLWrapper {
   String query;
   private Limit  limit = new Limit();
   private Offset offset = new Offset();
+  private List<WrapTheWrapper> addedWrappers = new ArrayList<WrapTheWrapper>();
+
 
   public CQLWrapper() {
     super();
@@ -47,6 +52,24 @@ public class CQLWrapper {
     return this;
   }
 
+  public CQLWrapper addWrapper(CQLWrapper wrapper){
+    addWrapper(wrapper, "and");
+    return this;
+  }
+
+  /**
+   *
+   * @param wrapper
+   * @param operator - and / or
+   * @return
+   */
+  public CQLWrapper addWrapper(CQLWrapper wrapper, String operator){
+    if(wrapper.query != null && wrapper.field != null){
+      addedWrappers.add(new WrapTheWrapper(wrapper, operator));
+    }
+    return this;
+  }
+
   /**
    * @return a space followed by the SQL clauses of WHERE, OFFSET and LIMIT
    * @throws IllegalStateException when the underlying CQL2PgJSON throws a QueryValidationException
@@ -54,14 +77,43 @@ public class CQLWrapper {
   @Override
   public String toString() {
     String q = "";
+    StringBuffer sb = new StringBuffer();
     if(query != null && field != null){
       try {
-        q = " WHERE " + field.cql2pgJson(query);
+        sb.append(field.cql2pgJson(query));
       } catch (QueryValidationException e) {
         throw new IllegalStateException(e);
       }
     }
+    int addons = addedWrappers.size();
+    for (int i = 0; i < addons; i++) {
+      CQL2PgJSON field = addedWrappers.get(i).wrapper.getField();
+      String query = addedWrappers.get(i).wrapper.getQuery();
+      if(sb.length() > 0){
+        sb.append(" ").append(addedWrappers.get(i).operator).append(" ");
+      }
+      try {
+        sb.append(field.cql2pgJson(query));
+      } catch (QueryValidationException e) {
+        throw new IllegalStateException(e);
+      }
+    }
+    if(sb.length() > 0){
+      q = " WHERE " + sb.toString();
+    }
     return  q + " " + offset.toString() + " " + limit.toString();
+  }
+
+  class WrapTheWrapper {
+
+    CQLWrapper wrapper;
+    String operator;
+
+    public WrapTheWrapper(CQLWrapper wrapper, String operator) {
+      this.wrapper = wrapper;
+      this.operator = operator;
+    }
+
   }
 
 }
