3a4ecda3fa25972ebc46c8a0e2dc25eb27d9000c
Julian Ladisch <julianladisch@users.noreply.github.com>
2017-03-22 19:20:24 +0100

    DMOD-218 replace CQLWrapper's IllegalStateException by CQLQueryValidationException

--- domain-models-runtime/src/test/java/org/folio/rest/persist/cql/CQLWrapperTest.java
+++ domain-models-runtime/src/test/java/org/folio/rest/persist/cql/CQLWrapperTest.java
@@ -2,6 +2,7 @@ package org.folio.rest.persist.cql;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.fail;
 
 import org.folio.rest.persist.cql.CQLWrapper;
 import org.junit.Test;
@@ -18,7 +19,7 @@ public class CQLWrapperTest {
   @Test
   public void allRecords() throws FieldException {
     CQLWrapper wrapper = new CQLWrapper().setField(new CQL2PgJSON("field")).setQuery("cql.allRecords=1");
-    assertThat(wrapper.toString(), matchesPattern("^ WHERE true *$"));
+    assertThat(wrapper.toString(), is(" WHERE true"));
   }
 
   @Test(expected = IllegalStateException.class)
@@ -26,4 +27,25 @@ public class CQLWrapperTest {
     CQLWrapper wrapper = new CQLWrapper().setField(new CQL2PgJSON("field")).setQuery("or name=miller");
     wrapper.toString();
   }
+
+  @Test
+  public void invalidCQLvalidation() throws FieldException {
+    CQLWrapper wrapper = new CQLWrapper().setField(new CQL2PgJSON("field")).setQuery("or name=miller");
+    try {
+      wrapper.toString();
+      fail("exception expected");
+    }
+    catch (CQLQueryValidationException e) {
+      assertThat(e.getMessage(), allOf(containsString("ParseException"), containsString("unexpected relation")));
+    }
+  }
+
+  @Test
+  public void wrap() throws FieldException {
+    CQLWrapper wrapper = new CQLWrapper().setField(new CQL2PgJSON("field")).setQuery("cql.allRecords=1");
+    wrapper.addWrapper(wrapper);
+    wrapper.addWrapper(wrapper, "or");
+    assertThat(wrapper.toString(), is(" WHERE true and true or true"));
+  }
+
 }
