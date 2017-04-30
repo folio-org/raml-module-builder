590339deac2332aa40f3696f39bf9602a96df70d
shale <shale99@users.noreply.github.com>
2017-04-30 22:09:49 +0300

    add more tests

--- domain-models-runtime/src/test/java/org/folio/rest/tools/utils/ResponseTest.java
+++ domain-models-runtime/src/test/java/org/folio/rest/tools/utils/ResponseTest.java
@@ -1,6 +1,9 @@
 package org.folio.rest.tools.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.apache.commons.io.IOUtils;
 import org.folio.rest.tools.client.Response;
@@ -33,11 +36,24 @@ public class ResponseTest {
       test1.setBody(j1);
       Response test2 = new Response();
       test2.setBody(j2);
+
       JsonPathParser jpp = new JsonPathParser( test1.joinOn("c.a1", test2, "a", "c.arr[1]").getBody() );
       assertEquals(jpp.getValueAt("c.a1.ignore"), "true");
 
+      //check non existent path
+      assertNull(jpp.getValueAt("c.a1.ignores!SA"));
+
+      //non existent insertField
+      assertNull(new JsonPathParser(test1.joinOn("c.a1", test2, "a", "c.arr[5]").getBody())
+        .getValueAt("a.c.a1"));
+
+      //non existant insertField and join field -
+      assertNotNull(test1.joinOn("c.a9", test2, "a", "c.arr[5]"));
+      //System.out.println(test1.joinOn("c.a9", test2, "a", "c.arr[5]").getBody());
+
     } catch (Exception e) {
       e.printStackTrace();
+      assertTrue(false);
     }
   }
 
