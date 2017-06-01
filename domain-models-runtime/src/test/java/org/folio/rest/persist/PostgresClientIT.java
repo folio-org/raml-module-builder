3340909e8ed6d781e0619516e3ec9c5938c763cc
Julian Ladisch <julianladisch@users.noreply.github.com>
2017-06-01 11:12:11 +0200

    tests for tenant separation and parallel execution

--- domain-models-runtime/src/test/java/org/folio/rest/persist/PostgresClientIT.java
+++ domain-models-runtime/src/test/java/org/folio/rest/persist/PostgresClientIT.java
@@ -1,11 +1,16 @@
 package org.folio.rest.persist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -127,23 +132,91 @@ public class PostgresClientIT {
     PostgresClient.getInstance(vertx, PostgresClient.DEFAULT_SCHEMA);
   }
 
-  @Test
-  public void tenantSeparation(TestContext context) {
+  private void createSchema(TestContext context, String tenant) {
     Async async = context.async();
-    PostgresClient c1 = PostgresClient.getInstance(vertx, TENANT);
-    PostgresClient c2 = PostgresClient.getInstance(vertx, TENANT2);
+    String schema = PostgresClient.convertToPsqlStandard(tenant);
+    String sql =
+        "CREATE ROLE " + schema + " PASSWORD '" + tenant + "' NOSUPERUSER NOCREATEDB INHERIT LOGIN;\n"
+      + "GRANT " + schema + " TO CURRENT_USER;\n"
+      + "CREATE SCHEMA " + schema + " AUTHORIZATION " + schema + ";\n"
+      + "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA " + schema + " TO " + schema + ";\n";
+    PostgresClient.getInstance(vertx).runSQLFile(sql, false, reply -> {
+      context.assertTrue(reply.succeeded());
+      async.complete();
+    });
+    async.await();
+  }
 
-    String sql1 =
-          "create table " + TENANT + ".a ( i integer );\n"
-        + "insert into "  + TENANT + ".a (i) values (5);\n";
-    c1.runSQLFile(sql1, true, reply1 -> {
+  private void createTable(TestContext context, PostgresClient client, String tenant, int i) {
+    Async async = context.async();
+    String schema = PostgresClient.convertToPsqlStandard(tenant);
+    String sql =
+        "drop table if exists " + schema + ".a;\n"
+      + "create table " + schema + ".a ( i integer );\n"
+      + "insert into "  + schema + ".a (i) values (" + i + ");\n";
+    client.runSQLFile(sql, true, reply1 -> {
       context.assertTrue(reply1.succeeded());
-      String sql2 = "select i from " + TENANT + ".a";
-      c2.runSQLFile(sql2, true, reply2 -> {
-        context.assertTrue(reply2.failed());
-        context.assertEquals("", reply2.cause().getMessage());
+      client.select("select i from " + schema + ".a", reply2 -> {
+        context.assertTrue(reply2.succeeded());
+        context.assertEquals(i, reply2.result().getResults().get(0).getInteger(0));
         async.complete();
       });
+    });
+    async.await();
+  }
+
+  private void selectFail(TestContext context, PostgresClient client, String tenant) {
+    Async async = context.async();
+    String schema = PostgresClient.convertToPsqlStandard(tenant);
+    client.select("select i from " + schema + ".a", reply -> {
+      context.assertFalse(reply.succeeded());
+      async.complete();
+    });
+    async.await();
+  }
+
+  @Test
+  public void tenantSeparation(TestContext context) {
+    String tenant = "tenantSeparation";
+    String tenant2 = "tenantSeparation2";
+    createSchema(context, tenant);
+    createSchema(context, tenant2);
+    PostgresClient c1 = PostgresClient.getInstance(vertx, tenant);
+    PostgresClient c2 = PostgresClient.getInstance(vertx, tenant2);
+    createTable(context, c1, tenant, 5);
+    createTable(context, c2, tenant2, 8);
+    // c1 must be blocked from accessing schema TENANT2
+    selectFail(context, c1, tenant2);
+    // c2 must be blocked from accessing schema TENANT
+    selectFail(context, c2, tenant);
+  }
+
+  @Test
+  public void parallel(TestContext context) {
+    int n = 10;
+    /** sleep time in milliseconds */
+    double sleep = 100;
+    /** maximum duration in milliseconds for the completion of all parallel queries */
+    long maxDuration = (long) (n * sleep / 2);
+    /* create n queries in parallel, each sleeping for some time.
+     * If vert.x properly processes them in parallel it finishes
+     * in less than half of the time needed for sequential processing.
+     */
+    Async async = context.async();
+    PostgresClient client = PostgresClient.getInstance(vertx);
+    List<Future> futures = new ArrayList<>(n);
+    for (int i=0; i<n; i++) {
+      Future<List<String>> future = Future.future();
+      client.runSQLFile("pg_sleep(" + sleep/1000 + ")", true, future.completer());
+      futures.add(future);
+    }
+    long start = System.currentTimeMillis();
+    CompositeFuture.all(futures).setHandler(handler -> {
+      context.assertTrue(handler.succeeded());
+      long duration = System.currentTimeMillis() - start;
+      context.assertTrue(duration < maxDuration,
+          "duration must be less than " + maxDuration + " ms, it is " + duration + " ms");
+      async.complete();
     });
   }
 }
