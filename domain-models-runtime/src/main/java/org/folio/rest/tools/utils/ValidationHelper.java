d7d7a67c8fb14535e91278ad5ad7151003aa254b
shale <shale99@users.noreply.github.com>
2018-04-25 11:37:23 +0300

    SQ

--- domain-models-runtime/src/main/java/org/folio/rest/tools/utils/ValidationHelper.java
+++ domain-models-runtime/src/main/java/org/folio/rest/tools/utils/ValidationHelper.java
@@ -23,6 +23,7 @@ public class ValidationHelper {
   private static final Messages MESSAGES = Messages.getInstance();
 
   private static final String QUERYEXCEPTION = "QueryValidationException";
+  private static final String CONTENT_TYPE = "Content-Type";
 
   public static Errors createValidationErrorMessage(String field, String value, String message){
     Errors e = new Errors();
@@ -51,8 +52,8 @@ public class ValidationHelper {
         String mess = errorMessageMap.getOrDefault('M', "");
         try{
           if(isInvalidUUID(mess)){
-            int start = mess.indexOf("\"");
-            int end = mess.indexOf("\"", start+1);
+            int start = mess.indexOf('\"');
+            int end = mess.indexOf('\"', start+1);
             String value = "";
             if(start != -1 && end != -1 && start < end){
               value = mess.substring(start+1, end);
@@ -61,8 +62,8 @@ public class ValidationHelper {
           }
           else if(isDuplicate(mess) || isFKViolation(mess)){
             String[] errorDesc = desc.split("=");
-            String field = errorDesc[0].substring(errorDesc[0].indexOf("(")+1,errorDesc[0].indexOf(")"));
-            String value = errorDesc[1].substring(errorDesc[1].indexOf("(")+1,errorDesc[1].indexOf(")"));
+            String field = errorDesc[0].substring(errorDesc[0].indexOf('(')+1,errorDesc[0].indexOf(')'));
+            String value = errorDesc[1].substring(errorDesc[1].indexOf('(')+1,errorDesc[1].indexOf(')'));
             r = withJsonUnprocessableEntity(ValidationHelper.createValidationErrorMessage(field, value, mess));
           }
           else if(isAuthFailed(mess)){
@@ -96,20 +97,19 @@ public class ValidationHelper {
             error.getLocalizedMessage()));
       }
       else if(error.getCause().getClass().getSimpleName().endsWith(QUERYEXCEPTION)){
-        String field = error.getMessage();
-        try {
-          int start = error.getMessage().indexOf("'");
-          int end = error.getMessage().lastIndexOf("'");
+        String message = error.getCause().getMessage();
+        String field = "";
+        if(message != null){
+          int start = message.indexOf('\'');
+          int end = message.lastIndexOf('\'');
           if(start != -1 && end != -1){
-            field = field.substring(start+1, end);
+            field = message.substring(start+1, end);
           }
-          if(field.contains(QUERYEXCEPTION)){
-            field = field.substring(field.indexOf(QUERYEXCEPTION)+QUERYEXCEPTION.length()+1);
-          }
-        } catch (Exception e1) {
-          e1.printStackTrace();
         }
-        Errors e = ValidationHelper.createValidationErrorMessage(field, "", error.getMessage());
+        else{
+          message = "";
+        }
+        Errors e = ValidationHelper.createValidationErrorMessage(field, "", message);
         return withJsonUnprocessableEntity(e);
       }
       else{
@@ -124,55 +124,39 @@ public class ValidationHelper {
   }
 
   private static Response withJsonUnprocessableEntity(Errors entity) {
-    Response.ResponseBuilder responseBuilder = Response.status(422).header("Content-Type", "application/json");
+    Response.ResponseBuilder responseBuilder = Response.status(422).header(CONTENT_TYPE, "application/json");
     responseBuilder.entity(entity);
     return responseBuilder.build();
   }
 
   private static Response withPlainInternalServerError(String entity) {
-    Response.ResponseBuilder responseBuilder = Response.status(500).header("Content-Type", "text/plain");
+    Response.ResponseBuilder responseBuilder = Response.status(500).header(CONTENT_TYPE, "text/plain");
     responseBuilder.entity(entity);
     return responseBuilder.build();
   }
 
   private static Response withForbiddenEntity() {
-    Response.ResponseBuilder responseBuilder = Response.status(401).header("Content-Type", "text/plain");
+    Response.ResponseBuilder responseBuilder = Response.status(401).header(CONTENT_TYPE, "text/plain");
     responseBuilder.entity("Unauthorized");
     return responseBuilder.build();
   }
 
   public static boolean isInvalidUUID(String errorMessage){
-    if(errorMessage != null &&
+    return (errorMessage != null &&
         (errorMessage.contains("invalid input syntax for type uuid") /*postgres v10*/ ||
-            errorMessage.contains("invalid input syntax for uuid") /*postgres v9.6*/)){
-      return true;
-    }
-    else{
-      return false;
-    }
+            errorMessage.contains("invalid input syntax for uuid") /*postgres v9.6*/));
   }
 
   public static boolean isFKViolation(String errorMessage){
-    if(errorMessage != null &&
-        (errorMessage.contains("violates foreign key constraint"))){
-      return true;
-    }
-    else{
-      return false;
-    }
+    return (errorMessage != null &&
+        (errorMessage.contains("violates foreign key constraint")));
   }
 
   public static boolean isDuplicate(String errorMessage){
-    if(errorMessage != null && errorMessage.contains("duplicate key value violates unique constraint")){
-      return true;
-    }
-    return false;
+    return (errorMessage != null && errorMessage.contains("duplicate key value violates unique constraint"));
   }
 
   public static boolean isAuthFailed(String errorMessage){
-    if(errorMessage != null && errorMessage.contains("password authentication failed for user")){
-      return true;
-    }
-    return false;
+    return (errorMessage != null && errorMessage.contains("password authentication failed for user"));
   }
 }
