2ec8c852c114824e02e4aa5f89e5136eaaa8fefb
Julian Ladisch <julianladisch@users.noreply.github.com>
2018-08-11 22:12:08 +0200

    RMB-191: Reenable JUnit 5

--- domain-models-interface-extensions/src/test/java/org/folio/rest/tools/AnnotationGrabberTest.java
+++ domain-models-interface-extensions/src/test/java/org/folio/rest/tools/AnnotationGrabberTest.java
@@ -5,17 +5,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 import org.junit.jupiter.api.Test;
 
 public class AnnotationGrabberTest {
+  private static final Logger log = LoggerFactory.getLogger(AnnotationGrabberTest.class);
+
   @Test
   void generateMappings() throws Exception {
     JsonObject mappings = AnnotationGrabber.generateMappings();
-    System.err.println(mappings.encodePrettily());
-    JsonObject unittests = mappings.getJsonObject("^/unittests");
+    log.debug(mappings.encodePrettily());
+    JsonObject unittests = mappings.getJsonObject("^unittests");
     assertThat(unittests.getString("class"), is("org.folio.rest.jaxrs.resource.TestResource"));
-    JsonArray books = unittests.getJsonArray("^/unittests/books/?$");
+    JsonArray books = unittests.getJsonArray("^unittestsbooks/?$");
     JsonObject book0 = books.getJsonObject(0);
     assertThat(book0.getString("function"), is("getRmbtestsBooks"));
     JsonObject params = book0.getJsonObject("params");
