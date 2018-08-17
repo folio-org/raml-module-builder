bbdea10ba5e2fb81f8985a80d4bf78b61d35490e
Julian Ladisch <julianladisch@users.noreply.github.com>
2018-08-17 11:59:19 +0200

    FolioLog4j2LogDelegate: extend javadoc; remove duplicate javadoc

--- util/src/main/java/io/vertx/core/logging/FolioLog4j2LogDelegate.java
+++ util/src/main/java/io/vertx/core/logging/FolioLog4j2LogDelegate.java
@@ -11,6 +11,23 @@ import io.vertx.core.logging.Log4j2LogDelegate;
  * <p>
  * Example for an Exception e: logger.error(e) is the same as logger.error(e.getMessage, e).
  * Use logger.error(e.toString()) or logger.error(e.getMessage()) to suppress the stacktrace.
+ * <p>
+ * Configure for a single logger:
+ * <pre>
+ * {@code
+ * Logger log = new Logger(new FolioLog4j2LogDelegate(getClass().getName()));
+ * }
+ * </pre>
+ * <p>
+ * Configure globally:
+ * <pre>
+ * {@code
+ *   static {
+ *    System.setProperty(LoggerFactory.LOGGER_DELEGATE_FACTORY_CLASS_NAME,
+ *      "io.vertx.core.logging.FolioLog4j2LogDelegateFactory");
+ *   }
+ * }
+ * </pre>
  */
 public class FolioLog4j2LogDelegate extends Log4j2LogDelegate {
   public FolioLog4j2LogDelegate(final String name) {
