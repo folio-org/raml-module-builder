202b79523e3f9e8b7eb9d8b4ed20f0e8f4e5753a
julianladisch <julianladisch@users.noreply.github.com>
2018-09-27 11:25:08 +0200

    RMB-252: Document security issues about reflection in UtilityClassTester (#275)
    * RMB-252: Document security issues about reflection in UtilityClassTester

--- testing/src/main/java/org/folio/rest/testing/UtilityClassTester.java
+++ testing/src/main/java/org/folio/rest/testing/UtilityClassTester.java
@@ -19,7 +19,17 @@ public final class UtilityClassTester {
    * Assert that the private constructor throws an UnsupportedOperationException.
    * @param constructor  Constructor of a utility class
    */
-  @java.lang.SuppressWarnings("squid:S1166")  // ignore "Either log or rethrow this exception"
+  @SuppressWarnings({
+    "squid:S1166",  /* suppress "Either log or rethrow this exception" */
+    "squid:S3011",  /* suppress "Changing accessibility is security sensitive" and
+                       suppress "Make sure that this accessibility update is safe here."
+                       This is save because
+                       - it runs during unit tests only, and
+                       - it invokes the constructor only, no write access, no i/o.
+                       There is no other way to test whether a private constructor
+                       throws an Exception.
+                     */
+  })
   private static void assertInvocationException(Constructor<?> constructor) {
     try {
       constructor.setAccessible(true);
