40eb0b67d5e74165b700bd2077f220bd7921a783
William Welling <wwelling@library.tamu.edu>
2018-10-16 15:38:03 -0500

    add result set closed handler to check for more results

--- domain-models-runtime/src/main/java/org/folio/rest/persist/PostgresClient.java
+++ domain-models-runtime/src/main/java/org/folio/rest/persist/PostgresClient.java
@@ -1425,7 +1425,9 @@ public class PostgresClient {
 
             String idPropName = databaseFieldToPojoSetter(idField);
 
-            sqlRowStream.handler(r -> {
+            sqlRowStream.resultSetClosedHandler(v -> {
+              sqlRowStream.moreResults();
+            }).handler(r -> {
               JsonObject row = convertRowStreamArrayToObject(sqlRowStream, r);
               try {
                 streamHandler.handle((T) deserializeRow(resultsHelper, externalColumnSettters, isAuditFlavored, idPropName, row));
