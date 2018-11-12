2d3c6cda56f99551fd2a544fb90b1bcd760ae2d9
William Welling <wwelling@library.tamu.edu>
2018-11-12 10:51:48 -0600

    serving up machine readable json schema and raml as json

--- domain-models-runtime/src/main/java/org/folio/rest/impl/JsonSchemaAPI.java
+++ domain-models-runtime/src/main/java/org/folio/rest/impl/JsonSchemaAPI.java
@@ -3,22 +3,21 @@ package org.folio.rest.impl;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URISyntaxException;
+import java.lang.StringBuffer;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.JarFile;
 import java.util.jar.JarEntry;
-import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.ws.rs.core.Response;
 
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.resource.JsonSchema;
 import org.folio.rest.tools.utils.ObjectMapperTool;
-
-import org.apache.commons.io.IOUtils;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
@@ -28,11 +27,12 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonSchemaAPI implements JsonSchema {
 
   private static final Logger log = LoggerFactory.getLogger(JsonSchemaAPI.class);
+
+  private static final Pattern REF_MATCH_PATTERN = Pattern.compile("\\\"\\$ref\\\"\\s*:\\s*\\\"(.*?)\\\"");
 
   @Validate
   @Override
@@ -68,13 +68,14 @@ public class JsonSchemaAPI implements JsonSchema {
     Handler<AsyncResult<Response>> asyncResultHandler,
     Context vertxContext
   ) {
+    String okapiUrl = okapiHeaders.get("x-okapi-url");
     vertxContext.runOnContext(v -> {
       try {
-        JsonNode schema = getSchemaByName(name);
-        if(schema != null) {
+        JsonNode schemaNode = getSchemaByName(name, okapiUrl);
+        if(schemaNode != null) {
           asyncResultHandler.handle(
             Future.succeededFuture(
-              GetJsonSchemaByNameResponse.respond200WithApplicationJson(schema)
+              GetJsonSchemaByNameResponse.respond200WithApplicationJson(schemaNode)
             )
           );
         } else {
@@ -123,24 +124,20 @@ public class JsonSchemaAPI implements JsonSchema {
     return schemas;
   }
 
-  private JsonNode getSchemaByName(String name) throws IOException {
-    log.info("\n\n\n\n{}", name);
-    JsonNode schema = null;
+  private JsonNode getSchemaByName(String name, String okapiUrl) throws IOException {
+    JsonNode schemaNode = null;
     File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
     JarFile jar = new JarFile(jarFile);
     Enumeration<JarEntry> entries = jar.entries();
     while(entries.hasMoreElements()) {
       JarEntry entry = entries.nextElement();
       String entryName = entry.getName();
-      log.info("  {}", entryName);
       if (entryName.startsWith("ramls/")) {
         String schemaName = entryName.substring(entryName.lastIndexOf("/") + 1);
-        log.info("    {}", schemaName);
         if(schemaName.equals(name)) {
-          log.info("** match ** ");
           try {
             InputStream is = jar.getInputStream(entry);
-            schema = ObjectMapperTool.getMapper().readValue(is, JsonNode.class);
+            schemaNode = replaceReferences(ObjectMapperTool.getMapper().readValue(is, JsonNode.class), okapiUrl);
             is.close();
             break;
           } catch(Exception e) {
@@ -150,8 +147,21 @@ public class JsonSchemaAPI implements JsonSchema {
       }
     }
     jar.close();
-    log.info("\n\n\n\n");
-    return schema;
+    return schemaNode;
+  }
+
+  private JsonNode replaceReferences(JsonNode schemaNode, String okapiUrl) throws Exception {
+    String schema = schemaNode.toString();
+    Matcher matcher = REF_MATCH_PATTERN.matcher(schema);
+    StringBuffer sb = new StringBuffer(schema.length());
+    while (matcher.find()) {
+      String ref =  matcher.group(1).substring(matcher.group(1).lastIndexOf("/") + 1);
+      if(!ref.startsWith("#")) {
+        matcher.appendReplacement(sb, Matcher.quoteReplacement("\"$ref\":\"" + okapiUrl + "/_/jsonSchema/" + ref + "\""));
+      }
+    }
+    matcher.appendTail(sb);
+    return ObjectMapperTool.getMapper().readValue(sb.toString(), JsonNode.class);
   }
 
 }
--- domain-models-runtime/src/main/java/org/folio/rest/impl/RamlAPI.java
+++ domain-models-runtime/src/main/java/org/folio/rest/impl/RamlAPI.java
@@ -3,20 +3,20 @@ package org.folio.rest.impl;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URISyntaxException;
+import java.lang.StringBuffer;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.JarFile;
 import java.util.jar.JarEntry;
-import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.ws.rs.core.Response;
 
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.resource.Raml;
-import org.folio.rest.tools.utils.ObjectMapperTool;
 
 import org.apache.commons.io.IOUtils;
 
@@ -29,10 +29,15 @@ import io.vertx.core.logging.LoggerFactory;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 public class RamlAPI implements Raml {
 
   private static final Logger log = LoggerFactory.getLogger(RamlAPI.class);
+
+  private static final Pattern INCLUDE_MATCH_PATTERN = Pattern.compile("(?<=!include ).*");
+
+  private static final ObjectMapper YAML_MAPPER = new ObjectMapper(new YAMLFactory());
 
   @Validate
   @Override
@@ -68,13 +73,14 @@ public class RamlAPI implements Raml {
     Handler<AsyncResult<Response>> asyncResultHandler,
     Context vertxContext
   ) {
+    String okapiUrl = okapiHeaders.get("x-okapi-url");
     vertxContext.runOnContext(v -> {
       try {
-        String raml = getRamlByName(name);
-        if(raml != null) {
+        JsonNode ramlNode = getRamlByName(name, okapiUrl);
+        if(ramlNode != null) {
           asyncResultHandler.handle(
             Future.succeededFuture(
-              GetRamlByNameResponse.respond200WithTextPlain(raml)
+              GetRamlByNameResponse.respond200WithApplicationJson(ramlNode)
             )
           );
         } else {
@@ -109,7 +115,9 @@ public class RamlAPI implements Raml {
         if(!ramlPath.contains("/")) {
           String ramlName = ramlPath.substring(ramlPath.lastIndexOf("/") + 1);
           try {
-            // TODO: validate raml file
+            InputStream is = jar.getInputStream(entry);
+            YAML_MAPPER.readValue(is, JsonNode.class);
+            is.close();
             ramls.add(ramlName);
           } catch(Exception e) {
             log.info("{} is not a valid raml file", entryName);
@@ -121,25 +129,20 @@ public class RamlAPI implements Raml {
     return ramls;
   }
 
-  private String getRamlByName(String name) throws IOException {
-    log.info("\n\n\n\n{}", name);
-    String raml = null;
+  private JsonNode getRamlByName(String name, String okapiUrl) throws IOException {
+    JsonNode ramlNode = null;
     File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
     JarFile jar = new JarFile(jarFile);
     Enumeration<JarEntry> entries = jar.entries();
     while(entries.hasMoreElements()) {
       JarEntry entry = entries.nextElement();
       String entryName = entry.getName();
-      log.info("  {}", entryName);
       if (entryName.startsWith("ramls/")) {
         String ramlName = entryName.substring(entryName.lastIndexOf("/") + 1);
-        log.info("    {}", ramlName);
         if(ramlName.equals(name)) {
-          log.info("** match ** ");
           try {
-            // TODO: validate raml file
             InputStream is = jar.getInputStream(entry);
-            raml = IOUtils.toString(is, "UTF-8");
+            ramlNode = replaceReferences(IOUtils.toString(is, "UTF-8"), okapiUrl);
             is.close();
             break;
           } catch(Exception e) {
@@ -149,8 +152,22 @@ public class RamlAPI implements Raml {
       }
     }
     jar.close();
-    log.info("\n\n\n\n");
-    return raml;
+    return ramlNode;
+  }
+
+  private JsonNode replaceReferences(String raml, String okapiUrl) throws Exception {
+    Matcher matcher = INCLUDE_MATCH_PATTERN.matcher(raml);
+    StringBuffer sb = new StringBuffer(raml.length());
+    while (matcher.find()) {
+      String ref =  matcher.group(0).substring(matcher.group(0).lastIndexOf("/") + 1);
+      if(ref.endsWith(".raml")) {
+        matcher.appendReplacement(sb, Matcher.quoteReplacement(okapiUrl + "/_/raml/" + ref));
+      } else {
+        matcher.appendReplacement(sb, Matcher.quoteReplacement(okapiUrl + "/_/jsonSchema/" + ref));
+      }
+    }
+    matcher.appendTail(sb);
+    return YAML_MAPPER.readValue(sb.toString(), JsonNode.class);
   }
 
 }
