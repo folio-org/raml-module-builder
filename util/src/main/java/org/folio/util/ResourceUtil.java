9731fde3998d159e594ee367b77672a3ea939191
Julian Ladisch <julianladisch@users.noreply.github.com>
2018-11-14 23:22:10 +0100

    RMB-300: optional leading slash for ResourceUtil::asString

--- util/src/main/java/org/folio/util/ResourceUtil.java
+++ util/src/main/java/org/folio/util/ResourceUtil.java
@@ -3,6 +3,7 @@ package org.folio.util;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Load a resource file as String.
@@ -15,22 +16,23 @@ public final class ResourceUtil {
   /**
    * Return the UTF-8 encoded resource file.
    * <p>
-   * asString("dir/file.txt") loads the resource src/main/resources/dir/file.txt
+   * Both asString("/dir/file.txt") and asString("dir/file.txt") load
+   * /dir/file.txt from the jar file or from classes/dir/file.txt or
+   * from test-classes/dir/file.txt.
    *
-   * @param name  resource path of the input file, without leading slash
-   * @param aClass  the class that provides the class loader
+   * @param name  resource path of the input file, with or without leading slash
    * @return the content of the resource file
    * @throws IOException on i/o error when reading the input file
    */
   public static String asString(final String name) throws IOException {
-    return asString(name, ResourceUtil.class.getClassLoader());
+    return asString(name, (ClassLoader) null);
   }
 
   /**
    * Return the UTF-8 encoded resource file using aClass' class loader
    * to locate it, for example when picking the .jar to search within.
    *
-   * @param name  resource path of the input file, without leading slash
+   * @param name  resource path of the input file, with or without leading slash
    * @param aClass  the class that provides the class loader
    * @return the content of the resource file
    * @throws IOException on i/o error when reading the input file
@@ -43,20 +45,31 @@ public final class ResourceUtil {
    * Return the UTF-8 encoded resource file using the classLoader
    * to locate it, for example when picking the .jar to search within.
    *
-   * @param name  resource path of the input file, without leading slash
+   * @param name  resource path of the input file, with or without leading slash
    * @param classLoader  the class loader that locates the resource file
    * @return the content of the resource file
    * @throws IOException on i/o error when reading the input file
    */
   public static String asString(final String name, ClassLoader classLoader) throws IOException {
-    ClassLoader finalClassLoader = classLoader;
-    if (classLoader == null) {
-      finalClassLoader = ResourceUtil.class.getClassLoader();
+    String finalName;
+    if (name.startsWith("/")) {
+      finalName = name;
+    } else {
+      finalName = "/" + name;
     }
-    try (InputStream inputStream = finalClassLoader.getResourceAsStream(name)) {
-      if (inputStream == null) {
-        throw new FileNotFoundException("Resource not found: " + name);
-      }
+
+    URL url;
+    if (classLoader == null) {
+      url = ResourceUtil.class.getResource(finalName);
+    } else {
+      url = classLoader.getResource(finalName);
+    }
+
+    if (url == null) {
+      throw new FileNotFoundException("Resource not found: " + name);
+    }
+
+    try (InputStream inputStream = url.openStream()) {
       return IoUtil.toStringUtf8(inputStream);
     }
   }
--- util/src/test/java/org/folio/util/ResourceUtilTest.java
+++ util/src/test/java/org/folio/util/ResourceUtilTest.java
@@ -4,11 +4,13 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.folio.rest.testing.UtilityClassTester;
 import org.junit.jupiter.api.Test;
+
+import io.vertx.core.Vertx;
 
 class ResourceUtilTest {
   @Test
@@ -23,22 +25,27 @@ class ResourceUtilTest {
 
   @Test
   void fileDoesNotExist() {
-    assertThrows(IOException.class, () -> ResourceUtil.asString("foobar"));
+    assertThrows(FileNotFoundException.class, () -> ResourceUtil.asString("/foobar"));
   }
 
   @Test
   void readEmptyFile() throws IOException {
-    assertThat(ResourceUtil.asString("ResourceUtilEmpty.bin"), is(""));
+    assertThat(ResourceUtil.asString("ResourceUtilEmpty.bin"),  is(""));
+    assertThat(ResourceUtil.asString("/ResourceUtilEmpty.bin"), is(""));
   }
 
   @Test
-  void readEmptyFileFromOtherClass() throws IOException {
-    assertThat(ResourceUtil.asString("ResourceUtilEmpty.bin", File.class), is(""));
+  void readFileFromOtherClass() {
+    assertThrows(FileNotFoundException.class,
+        () -> ResourceUtil.asString("ResourceUtilEmpty.bin",  Vertx.class));
+    assertThrows(FileNotFoundException.class,
+        () -> ResourceUtil.asString("/ResourceUtilEmpty.bin", Vertx.class));
   }
 
   @Test
   void readExampleFile() throws IOException {
-    assertThat(ResourceUtil.asString("ResourceUtilExample.bin"), is("first line\numlauts: äöü\n"));
+    assertThat(ResourceUtil.asString("ResourceUtilExample.bin"),  is("first line\numlauts: äöü\n"));
+    assertThat(ResourceUtil.asString("/ResourceUtilExample.bin"), is("first line\numlauts: äöü\n"));
   }
 
   @Test
@@ -47,6 +54,7 @@ class ResourceUtilTest {
     for (int i=0; i<expected.length; i++) {
       expected[i] = 'a';
     }
-    assertThat(ResourceUtil.asString("ResourceUtil3000.bin"), is(new String(expected)));
+    assertThat(ResourceUtil.asString("ResourceUtil3000.bin"),  is(new String(expected)));
+    assertThat(ResourceUtil.asString("/ResourceUtil3000.bin"), is(new String(expected)));
   }
 }
