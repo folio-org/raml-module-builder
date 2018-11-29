d7a96887f8d2eba0a1245f5330460c181c9cab1d
julianladisch <julianladisch@users.noreply.github.com>
2018-11-29 15:11:47 +0100

    RMB-288: Move StorageHelper to PgUtil - CRUD boilerplate code (#335)
    * RMB-288: Move StorageHelper to PgUtil - CRUD boilerplate code
    
    * Extract duplicated code into methods
    
    * DELETE, PUT: Return 404 if not found, 500 if more than 1 record modified.

---
+++ domain-models-runtime/src/test/java/org/folio/rest/jaxrs/model/UserdataCollection.java
@@ -0,0 +1,139 @@
+
+package org.folio.rest.jaxrs.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Collection of users
+ *
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "users",
+    "totalRecords",
+    "resultInfo"
+})
+public class UserdataCollection {
+
+    /**
+     * List of userdata items
+     * (Required)
+     *
+     */
+    @JsonProperty("users")
+    @JsonPropertyDescription("List of userdata items")
+    @Valid
+    @NotNull
+    private List<User> users = new ArrayList<User>();
+    /**
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("totalRecords")
+    @NotNull
+    private Integer totalRecords;
+    @JsonProperty("resultInfo")
+    @Null
+    @Valid
+    private ResultInfo resultInfo;
+    @JsonIgnore
+    @Valid
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * List of userdata items
+     * (Required)
+     *
+     */
+    @JsonProperty("users")
+    public List<User> getUsers() {
+        return users;
+    }
+
+    /**
+     * List of userdata items
+     * (Required)
+     *
+     */
+    @JsonProperty("users")
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public UserdataCollection withUsers(List<User> users) {
+        this.users = users;
+        return this;
+    }
+
+    /**
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("totalRecords")
+    public Integer getTotalRecords() {
+        return totalRecords;
+    }
+
+    /**
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("totalRecords")
+    public void setTotalRecords(Integer totalRecords) {
+        this.totalRecords = totalRecords;
+    }
+
+    public UserdataCollection withTotalRecords(Integer totalRecords) {
+        this.totalRecords = totalRecords;
+        return this;
+    }
+
+    @JsonProperty("resultInfo")
+    public ResultInfo getResultInfo() {
+        return resultInfo;
+    }
+
+    @JsonProperty("resultInfo")
+    public void setResultInfo(ResultInfo resultInfo) {
+        this.resultInfo = resultInfo;
+    }
+
+    public UserdataCollection withResultInfo(ResultInfo resultInfo) {
+        this.resultInfo = resultInfo;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public UserdataCollection withAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+        return this;
+    }
+
+}
