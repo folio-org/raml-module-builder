30bea50d786513887ca0b47ed32216d1d0c2792c
Siarhei_Hrabko <siarhei_hrabko@epam.com>
2019-01-04 16:24:17 +0300

    added PostgresClient.closeAllClients unit test

--- domain-models-runtime/src/test/java/org/folio/rest/persist/PostgresClientIT.java
+++ domain-models-runtime/src/test/java/org/folio/rest/persist/PostgresClientIT.java
@@ -67,7 +67,7 @@ public class PostgresClientIT {
     System.setProperty(LoggerFactory.LOGGER_DELEGATE_FACTORY_CLASS_NAME, "io.vertx.core.logging.Log4j2LogDelegateFactory");
   }
 
-  @BeforeClass
+ // @BeforeClass
   public static void doesNotCompleteOnWindows() {
     final String os = System.getProperty("os.name").toLowerCase();
     org.junit.Assume.assumeFalse(os.contains("win")); // RMB-261
@@ -1463,4 +1463,16 @@ public class PostgresClientIT {
     });
     async.awaitSuccess();
   }
+
+  @Test
+  public void closeAllClientsTest (TestContext testContext) throws Exception {
+    String embed = System.getProperty("embed_postgres", "").toLowerCase().trim();
+    if ("true".equals(embed)) {
+      PostgresClient.stopEmbeddedPostgres();
+      PostgresClient.setIsEmbedded(true);
+      PostgresClient.getInstance(vertx).startEmbeddedPostgres();
+    }
+    PostgresClient.getInstance(vertx).execute("SELECT 1", testContext.asyncAssertSuccess());
+  }
+
 }
