261acb63ad7c08002cc70db4431962aed99b0497
Adam Dickmeiss <adam@indexdata.dk>
2019-01-15 15:34:36 +0100

    Add unit tests for sample2

---
+++ sample2/src/test/java/org/folio/rest/impl/APITests.java
@@ -0,0 +1,111 @@
+package org.folio.rest.impl;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.internal.mapper.ObjectMapperType;
+import com.jayway.restassured.response.Header;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.folio.rest.RestVerticle;
+import org.folio.rest.jaxrs.model.User;
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class APITests {
+  private final int portCodex = 9230;
+  private Vertx vertx;
+  
+  private static Logger logger = LoggerFactory.getLogger("x");
+  private final Header tenantHeader = new Header("X-Okapi-Tenant", "testlib");
+  
+  private void setupMux(TestContext context) {
+    JsonObject conf = new JsonObject();
+    conf.put("http.port", portCodex);
+    DeploymentOptions opt = new DeploymentOptions()
+      .setConfig(conf);
+    vertx.deployVerticle(RestVerticle.class.getName(), opt, context.asyncAssertSuccess());
+  }
+  
+  
+  @Before
+  public void setUp(TestContext context) {
+    vertx = Vertx.vertx();
+    RestAssured.port = portCodex;
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+  
+    setupMux(context);
+  }
+  
+  @After
+  public void tearDown(TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+  }
+  
+  @Test
+  public void testBadContentType(TestContext context) {
+    RestAssured.given()
+        .header(tenantHeader)
+        .header(new Header("Content-Type", "application/json"))
+        .body("<hello/>")
+      .when()
+        .post("/users/1")
+      .then()
+        .contentType("")
+        .statusCode(400);
+  }
+
+  @Test
+  public void testEcho(TestContext context) {
+    RestAssured.given()
+        .header(tenantHeader)
+        .header(new Header("Content-Type", "application/xml"))
+        .body("<hello/>")
+      .when()
+        .post("/users/1")
+      .then()
+        .contentType("application/xml")
+        .statusCode(201).content(Matchers.equalTo("<hello/>"));
+  }
+
+  @Test
+  public void testEchoWithLanguage(TestContext context) {
+    User user = RestAssured.given()
+        .header(tenantHeader)
+      .when()
+        .get("/users/1?lang=en")
+      .then()
+        .contentType("application/json")
+        .statusCode(200).extract().as(User.class, ObjectMapperType.JACKSON_1);
+    context.assertEquals("John", user.getFirstname());
+    context.assertEquals(20, user.getAge());
+  }
+  @Test
+  public void testEchoWithBadLanguage(TestContext context) {
+    RestAssured.given()
+        .header(tenantHeader)
+      .when()
+        .get("/users/1?lang=dk")
+      .then()
+        .contentType("text/plain")
+        .statusCode(400).content(Matchers.equalTo("bad language"));
+  }
+
+  @Test
+  public void testEchoWithBadLanguage2(TestContext context) {
+    RestAssured.given()
+        .header(tenantHeader)
+      .when()
+        .get("/users/1?lang=ddd")
+      .then()
+        .contentType("")
+        .statusCode(400);
+  } 
+}
