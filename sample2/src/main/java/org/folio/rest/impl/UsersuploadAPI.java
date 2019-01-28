81fe865afcf1db7bd09b5d3285a26682b44a04c9
Adam Dickmeiss <adam@indexdata.dk>
2019-01-28 18:40:46 +0100

    Comment

--- sample2/src/main/java/org/folio/rest/impl/UsersuploadAPI.java
+++ sample2/src/main/java/org/folio/rest/impl/UsersuploadAPI.java
@@ -14,6 +14,7 @@ import org.folio.rest.jaxrs.resource.Usersupload;
 
 public class UsersuploadAPI implements Usersupload {
 
+  // message length for a stream
   private static Map<String, Integer> streams = new HashMap<>();
 
   @Stream
