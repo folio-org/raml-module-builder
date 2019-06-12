2e9dcdacc74b75102c41a028295a535eff810aee
Julian Ladisch <julianladisch@users.noreply.github.com>
2019-06-12 14:29:27 +0200

    Add uuidConstant() test case

--- cql2pgjson/src/test/java/org/z3950/zing/cql/cql2pgjson/ForeignKeyGenerationIT.java
+++ cql2pgjson/src/test/java/org/z3950/zing/cql/cql2pgjson/ForeignKeyGenerationIT.java
@@ -1,19 +1,20 @@
 package org.z3950.zing.cql.cql2pgjson;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
 
 import org.folio.cql2pgjson.CQL2PgJSON;
+import org.folio.cql2pgjson.exception.QueryValidationException;
 
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.assertThat;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import junitparams.JUnitParamsRunner;
-
 
 @RunWith(JUnitParamsRunner.class)
 public class ForeignKeyGenerationIT extends DatabaseTestBase {
@@ -23,7 +24,6 @@ public class ForeignKeyGenerationIT extends DatabaseTestBase {
   public static void runOnceBeforeClass() throws Exception {
     setupDatabase();
     runSqlFile("foreignKey.sql");
-
   }
 
   @AfterClass
@@ -37,29 +37,39 @@ public class ForeignKeyGenerationIT extends DatabaseTestBase {
     cql2pgJson.setDbSchemaPath("templates/db_scripts/foreignKey.json");
   }
 
+  private List<String> cql(String cql) {
+    try {
+      String sql = cql2pgJson.toSql(cql).toString();
+      return firstColumn("select jsonb->>'name' from tablea " + sql);
+    } catch (QueryValidationException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   @Test
   public void foreignKeySearch0() throws Exception {
-    String sql = cql2pgJson.toSql("tableb.prefix == x0").toString();
-    assertThat(firstColumn("select jsonb->>'name' from tablea " + sql), is(empty()));
+    assertThat(cql("tableb.prefix == x0"), is(empty()));
   }
 
   @Test
   public void foreignKeySearch1() throws Exception {
-    String sql = cql2pgJson.toSql("tableb.prefix == x1").toString();
-    assertThat(firstColumn("select jsonb->>'name' from tablea " + sql), containsInAnyOrder("test1"));
+    assertThat(cql("tableb.prefix == x1"), containsInAnyOrder("test1"));
   }
 
   @Test
   public void foreignKeySearch2() throws Exception {
-    String sql = cql2pgJson.toSql("tableb.prefix == x2").toString();
     // two tableb records match, but they both reference the same tablea record, therefore "test2" should be
     // returned one time only
-    assertThat(firstColumn("select jsonb->>'name' from tablea " + sql), containsInAnyOrder("test2"));
+    assertThat(cql("tableb.prefix == x2"), containsInAnyOrder("test2"));
   }
 
   @Test
   public void foreignKeyFilter1() throws Exception {
-    String sql = cql2pgJson.toSql("id == tableb.tableaId").toString();
-    assertThat(firstColumn("select jsonb->>'name' from tablea " + sql), containsInAnyOrder("test1", "test2"));
+    assertThat(cql("id == tableb.tableaId"), containsInAnyOrder("test1", "test2"));
+  }
+
+  @Test
+  public void uuidConstant() throws Exception {
+    assertThat(cql("tableb.name == 33333333-3333-3333-3333-333333333333"), is(empty()));
   }
 }
