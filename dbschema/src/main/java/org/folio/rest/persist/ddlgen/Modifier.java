5b44e2779ab635d6fa7b910482cc1dbddbeeb22f
Adam Dickmeiss <adam@indexdata.dk>
2019-06-20 15:25:38 +0200

    Missing Modifier.java

---
+++ dbschema/src/main/java/org/folio/rest/persist/ddlgen/Modifier.java
@@ -0,0 +1,23 @@
+package org.folio.rest.persist.ddlgen;
+
+public class Modifier {
+  private String modifierName;
+  private String subfield;
+
+  public String getModifierName() {
+    return modifierName;
+  }
+
+  public void setModifierName(String modifierName) {
+    this.modifierName = modifierName;
+  }
+
+  public String getSubfield() {
+    return subfield;
+  }
+
+  public void setSubfield(String subfield) {
+    this.subfield = subfield;
+  }
+  
+}
