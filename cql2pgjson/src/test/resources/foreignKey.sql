a155d680ee90fcd5ef8db8be72267c231280fde6
Eric Valuk <evaluk@ebsco.com>
2019-06-26 13:59:19 -0400

    brought back padding of tables

--- cql2pgjson/src/test/resources/foreignKey.sql
+++ cql2pgjson/src/test/resources/foreignKey.sql
@@ -58,12 +58,12 @@ INSERT INTO tablea (jsonb) VALUES
 ('{"id": "A2222222-2222-2222-2222-222222222222", "name": "test2","tabledId": "D2222222-2222-2222-2222-222222222222"}'),
 ('{"id": "A3333333-3333-3333-3333-333333333333", "name": "test3","tabledId": "D3333333-3333-3333-3333-333333333333"}'),
 ('{"id": "A4444444-4444-4444-4444-444444444444", "name": "test4"}');
-
+INSERT INTO tablea (jsonb) VALUES (jsonb_build_object('id', md5('a' || generate_series(1, 2000)::text)));
 INSERT INTO tableb (jsonb) VALUES
 ('{"id": "B1111111-1111-1111-1111-111111111111", "prefix": "x1", "otherindex": "y1","tableaId": "A1111111-1111-1111-1111-111111111111"}'),
 ('{"id": "B2222222-2222-2222-2222-222222222222", "prefix": "x2", "otherindex": "y2", "tableaId": "A2222222-2222-2222-2222-222222222222"}'),
 ('{"id": "B3333333-3333-3333-3333-333333333333", "prefix": "x2", "otherindex": "y3","tableaId": "A2222222-2222-2222-2222-222222222222"}'),
 ('{"id": "B4444444-4444-4444-4444-444444444444", "prefix" : "x0'')));(((''DROP tableb","otherindex": "y4", "tableaId": "A3333333-3333-3333-3333-333333333333"}');
-
+INSERT INTO tableb (jsonb) VALUES (jsonb_build_object('id', md5('b' || generate_series(1, 2000)::text)));
 
 
