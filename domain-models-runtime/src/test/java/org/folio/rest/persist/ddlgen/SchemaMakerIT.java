26bde98c02d15ac293a501d2cd69813ccf88ad8a
Julian Ladisch <julianladisch@users.noreply.github.com>
2019-07-16 16:33:34 +0200

    RMB-430: Generate audit id for concurrent queries.

--- domain-models-runtime/src/test/java/org/folio/rest/persist/ddlgen/SchemaMakerIT.java
+++ domain-models-runtime/src/test/java/org/folio/rest/persist/ddlgen/SchemaMakerIT.java
@@ -9,6 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.Timeout;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 import org.folio.rest.persist.PostgresClient;
@@ -16,11 +17,15 @@ import org.folio.rest.persist.PostgresClientITBase;
 import org.folio.rest.tools.utils.ObjectMapperTool;
 import org.folio.util.ResourceUtil;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(VertxUnitRunner.class)
 public class SchemaMakerIT extends PostgresClientITBase {
+  @Rule
+  public Timeout rule = Timeout.seconds(15);
+
   @Before
   public void wipeAll(TestContext context) {
     dropSchemaAndRole(context);
@@ -57,20 +62,25 @@ public class SchemaMakerIT extends PostgresClientITBase {
         " SELECT md5(username)::uuid, json_build_object('username', username, 'id', md5(username)::uuid)" +
         " FROM (SELECT 'user' || generate_series(1, 5) AS username) AS subquery");
     execute(context, "UPDATE " + table + " SET jsonb=jsonb_set(jsonb, '{foo}', to_jsonb('bar'::text))");
+    execute(context, "UPDATE " + table + " SET jsonb=jsonb_set(jsonb, '{foo}', to_jsonb('baz'::text))");
+    execute(context, "UPDATE " + table + " SET jsonb=jsonb_set(jsonb, '{foo}', to_jsonb('bar'::text))");
+    execute(context, "UPDATE " + table + " SET jsonb=jsonb_set(jsonb, '{foo}', to_jsonb('baz'::text))");
     execute(context, "DELETE FROM " + table);
 
     String auditTable = table.replace(".", ".audit_");
     String count = "SELECT count(*) FROM " + auditTable;
     assertThat("total number of audit entries",
-        selectInteger(context, count), is(15));
+        selectInteger(context, count), is(30));
     assertThat("total number of audit entries for insert",
         selectInteger(context, count + " WHERE jsonb->>'operation'='I' AND jsonb->'" + field + "'->>'foo' IS NULL"), is(5));
     assertThat("total number of audit entries for update",
-        selectInteger(context, count + " WHERE jsonb->>'operation'='U' AND jsonb->'" + field + "'->>'foo' = 'bar'"), is(5));
+        selectInteger(context, count + " WHERE jsonb->>'operation'='U' AND jsonb->'" + field + "'->>'foo' = 'bar'"), is(10));
+    assertThat("total number of audit entries for update",
+        selectInteger(context, count + " WHERE jsonb->>'operation'='U' AND jsonb->'" + field + "'->>'foo' = 'baz'"), is(10));
     assertThat("total number of audit entries for delete",
-        selectInteger(context, count + " WHERE jsonb->>'operation'='D' AND jsonb->'" + field + "'->>'foo' = 'bar'"), is(5));
+        selectInteger(context, count + " WHERE jsonb->>'operation'='D' AND jsonb->'" + field + "'->>'foo' = 'baz'"), is(5));
     assertThat("number of user2 audit entries",
-        selectInteger(context, count + " WHERE jsonb->'" + field + "'->>'id' = md5('user2')::uuid::text"), is(3));
+        selectInteger(context, count + " WHERE jsonb->'" + field + "'->>'id' = md5('user2')::uuid::text"), is(6));
   }
 
   @Test
@@ -83,6 +93,39 @@ public class SchemaMakerIT extends PostgresClientITBase {
     // Check that '"withAuditing": true' implicitly creates the table 'audit_test_implicit'
     // without an explicit entry in the schema.json table list
     auditedTableCanInsertUpdateDelete(context, schema + ".test_implicit", "implicitHistory");
+  }
+
+  @Test
+  public void canConcurrentlyUseAuditedTable(TestContext context) {
+    runSchema(context, TenantOperation.CREATE, "schemaWithAudit.json");
+    String table = schema + ".test_tenantapi";
+    execute(context, "INSERT INTO " + table +
+        " SELECT md5(username)::uuid, json_build_object('username', username, 'id', md5(username)::uuid)" +
+        " FROM (SELECT 'patron' || generate_series(1, 2) AS username) AS subquery");
+
+    // concurrently update two records: https://issues.folio.org/browse/RMB-430
+    Async async = context.async();
+    PostgresClient postgresClient = PostgresClient.getInstance(vertx, tenant);
+    postgresClient.startTx(tx1 -> {
+      context.assertTrue(tx1.succeeded());
+      postgresClient.startTx(tx2 -> {
+        context.assertTrue(tx2.succeeded());
+        String sql = "UPDATE " + table + " SET jsonb=jsonb_set(jsonb, '{x}', to_jsonb('y'::text)) WHERE jsonb->>'username' = ";
+        postgresClient.execute(tx1, sql + "'patron1'", context.asyncAssertSuccess(update1 -> {
+          postgresClient.execute(tx2, sql + "'patron2'", context.asyncAssertSuccess(update2 -> {
+            postgresClient.endTx(tx1, context.asyncAssertSuccess(endTx1 -> {
+              postgresClient.endTx(tx2, context.asyncAssertSuccess(endTx2 -> {
+                String sql3 = "SELECT * FROM " + table.replace(".", ".audit_");
+                postgresClient.select(sql3, context.asyncAssertSuccess(result -> {
+                  context.assertEquals(4, result.getNumRows());
+                  async.complete();
+                }));
+              }));
+            }));
+          }));
+        }));
+      });
+    });
   }
 
   private boolean triggerExists(TestContext context, String name) {
