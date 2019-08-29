63621957beeb8d41c8a8e4b56a4bdb5d47520ba9
Eric Valuk <evaluk@ebsco.com>
2019-08-29 09:51:58 -0400

    fix for newly broken unit tests due to code change

--- cql2pgjson/src/test/java/org/folio/cql2pgjson/CompoundIndexTest.java
+++ cql2pgjson/src/test/java/org/folio/cql2pgjson/CompoundIndexTest.java
@@ -11,7 +11,7 @@ public class CompoundIndexTest {
     CQL2PgJSON cql2pgJson = new CQL2PgJSON("tablea");
     cql2pgJson.setDbSchemaPath("templates/db_scripts/compoundIndexTest.json");
     String sql = cql2pgJson.toSql("fullname == John Smith").toString();
-    String expected = "WHERE lower(concat_ws(' ',firstName , lastName)) LIKE lower('John Smith')";
+    String expected = "WHERE lower(concat_ws(' ',tablea.jsonb->>firstName , tablea.jsonb->>lastName)) LIKE lower('John Smith')";
     assertEquals(expected, sql);
   }
 
@@ -20,7 +20,7 @@ public class CompoundIndexTest {
     CQL2PgJSON cql2pgJson = new CQL2PgJSON("tablea");
     cql2pgJson.setDbSchemaPath("templates/db_scripts/compoundIndexTest.json");
     String sql = cql2pgJson.toSql("ftfield = John Smith").toString();
-    String expected = "WHERE to_tsvector('simple', concat_ws(' ',field1 , field2)) @@ replace((to_tsquery('simple', ('''John''')) && to_tsquery('simple', ('''Smith''')))::text, '&', '<->')::tsquery";
+    String expected = "WHERE to_tsvector('simple', concat_ws(' ',.jsonb->>field1 , .jsonb->>field2)) @@ replace((to_tsquery('simple', ('''John''')) && to_tsquery('simple', ('''Smith''')))::text, '&', '<->')::tsquery";
     assertEquals(expected, sql);
   }
 
