2eabf1863612e695463f046c5040f8c1d071f7b8
Eric Valuk <evaluk@ebsco.com>
2019-09-27 16:00:33 -0400

    new Unit tests for index inside of dbschema

---
+++ dbschema/src/test/java/org/folio/rest/persist/ddlgen/IndexTest.java
@@ -0,0 +1,21 @@
+package org.folio.rest.persist.ddlgen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+class IndexTest {
+
+  @Test
+  void testIndexExpression() {
+    Index idx = new Index();
+    idx.setFieldName("testIdx");
+    idx.setMultiFieldNames("field1,field2,field3");
+    assertEquals("concat_space_sql(test_table.jsonb->>'field1',test_table.jsonb->>'field2',test_table.jsonb->>'field3')",idx.getFinalSqlExpression("test_table"));
+    idx.setFieldName("testIdx");
+    idx.setMultiFieldNames("blah.blah2.field1,blah.blah2.field2,blah.blah2.field3");
+    assertEquals("concat_space_sql(test_table.jsonb->blah->blah2->>'field1',test_table.jsonb->blah->blah2->>'field2',test_table.jsonb->blah->blah2->>'field3')",idx.getFinalSqlExpression("test_table"));
+  }
+}
