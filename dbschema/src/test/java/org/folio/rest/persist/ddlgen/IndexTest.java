939dd571a660fb6583bde5bdcbb2e7d1b93d214a
Eric Valuk <evaluk@ebsco.com>
2019-09-27 16:26:45 -0400

    more unit tests added to improve code coverage

--- dbschema/src/test/java/org/folio/rest/persist/ddlgen/IndexTest.java
+++ dbschema/src/test/java/org/folio/rest/persist/ddlgen/IndexTest.java
@@ -11,11 +11,18 @@ class IndexTest {
   @Test
   void testIndexExpression() {
     Index idx = new Index();
+    idx.setFieldPath("testIdx");
     idx.setFieldName("testIdx");
     idx.setMultiFieldNames("field1,field2,field3");
     assertEquals("concat_space_sql(test_table.jsonb->>'field1' , test_table.jsonb->>'field2' , test_table.jsonb->>'field3')",idx.getFinalSqlExpression("test_table"));
     idx.setFieldName("testIdx");
     idx.setMultiFieldNames("blah.blah2.field1,blah.blah2.field2,blah.blah2.field3");
     assertEquals("concat_space_sql(test_table.jsonb->'blah'->'blah2'->>'field1' , test_table.jsonb->'blah'->'blah2'->>'field2' , test_table.jsonb->'blah'->'blah2'->>'field3')",idx.getFinalSqlExpression("test_table"));
+    idx.setMultiFieldNames(null);
+    idx.setSqlExpression("concat_space_sql(test_table.jsonb->'blah'->'blah2'->>'field1' , test_table.jsonb->'blah'->'blah2'->>'field2' , test_table.jsonb->'blah'->'blah2'->>'field3'");
+    assertEquals("concat_space_sql(test_table.jsonb->'blah'->'blah2'->>'field1' , test_table.jsonb->'blah'->'blah2'->>'field2' , test_table.jsonb->'blah'->'blah2'->>'field3')",idx.getFinalSqlExpression("test_table"));
+    idx.setSqlExpression(null);
+    assertEquals("testIdx",idx.getFinalSqlExpression("test_table"));
+
   }
 }
