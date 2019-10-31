148a75efd03d02f1c925718e893e02757dc6e2f8
Eric Valuk <evaluk@ebsco.com>
2019-10-31 10:44:52 -0400

    test commit

--- dbschema/src/test/java/org/folio/rest/persist/ddlgen/IndexTest.java
+++ dbschema/src/test/java/org/folio/rest/persist/ddlgen/IndexTest.java
@@ -22,13 +22,13 @@ class IndexTest {
   }
   @ParameterizedTest
   @CsvSource({
-    "a[*].b  , concat_array_object_values(table.jsonb->'a','b')",
-    "a.b[*].c, concat_array_object_values(table.jsonb->'a'->'b','c')",
+    "a[*].b,concat_array_object_values(table.jsonb->'a','b')",
+    "a.b[*].c,concat_array_object_values(table.jsonb->'a'->'b','c')",
   })
   void appendExpandedArrayTerm(String term, String expectedSql) {
     StringBuilder result = new StringBuilder();
     Index.appendExpandedTerm( "table", term,result);
-    assertEquals(  expectedSql,result.toString());
+    assertEquals( result.toString() + "1",result.toString());
   }
   @Test
   void multiFieldNames() {
