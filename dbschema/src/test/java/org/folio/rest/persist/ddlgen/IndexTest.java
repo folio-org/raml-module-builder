655573e2c6103675dcd7e828957a33ab3f558976
Eric Valuk <evaluk@ebsco.com>
2019-10-31 11:53:03 -0400

    possible fix for escaping comma

--- dbschema/src/test/java/org/folio/rest/persist/ddlgen/IndexTest.java
+++ dbschema/src/test/java/org/folio/rest/persist/ddlgen/IndexTest.java
@@ -22,8 +22,8 @@ class IndexTest {
   }
   @ParameterizedTest
   @CsvSource({
-    "a[*].b,concat_array_object_values(table.jsonb->'a','b')",
-    "a.b[*].c,concat_array_object_values(table.jsonb->'a'->'b','c')",
+    "a[*].b,\"concat_array_object_values(table.jsonb->'a','b')\"",
+    "a.b[*].c,\"concat_array_object_values(table.jsonb->'a'->'b','c')\"",
   })
   void appendExpandedArrayTerm(String term, String expectedSql) {
     StringBuilder result = new StringBuilder();
