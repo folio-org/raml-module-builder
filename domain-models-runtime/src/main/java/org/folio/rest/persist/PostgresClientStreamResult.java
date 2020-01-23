394104fee31f9234c30aad61749596d9d4db04c1
Adam Dickmeiss <adam@indexdata.dk>
2020-01-23 16:52:14 +0100

    Whitespace

--- domain-models-runtime/src/main/java/org/folio/rest/persist/PostgresClientStreamResult.java
+++ domain-models-runtime/src/main/java/org/folio/rest/persist/PostgresClientStreamResult.java
@@ -4,24 +4,25 @@ import io.vertx.core.Handler;
 import org.folio.rest.jaxrs.model.ResultInfo;
 
 class PostgresClientStreamResult<T> {
+
   private final ResultInfo resultInfo;
-  
+
   private Handler<T> streamHandler;
   private Handler<Void> endHandler;
   private Handler<Throwable> exceptionHandler;
-  
+
   public PostgresClientStreamResult(ResultInfo resultInfo) {
     this.resultInfo = resultInfo;
   }
-  
+
   ResultInfo resultInto() {
     return this.resultInfo;
   }
-  
+
   public void handler(Handler<T> streamHandler) {
     this.streamHandler = streamHandler;
   }
-  
+
   public void endHandler(Handler<Void> endHandler) {
     this.endHandler = endHandler;
   }
