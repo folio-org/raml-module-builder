6f7f9927ad24fb5001ea5f6da7c5e0071e9d46b1
Adam Dickmeiss <adam@indexdata.dk>
2020-05-01 20:00:18 +0200

    missing import

--- domain-models-runtime/src/main/java/org/folio/rest/persist/PostgresClient.java
+++ domain-models-runtime/src/main/java/org/folio/rest/persist/PostgresClient.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
