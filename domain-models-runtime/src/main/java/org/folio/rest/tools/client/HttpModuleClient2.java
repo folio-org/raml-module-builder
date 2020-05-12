5a1414f2aff3e41453668c4010e40d52a8383b84
Adam Dickmeiss <adam@indexdata.dk>
2020-05-12 12:38:59 +0200

    Remove unused imports

--- domain-models-runtime/src/main/java/org/folio/rest/tools/client/HttpModuleClient2.java
+++ domain-models-runtime/src/main/java/org/folio/rest/tools/client/HttpModuleClient2.java
@@ -32,8 +32,6 @@ import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.http.HttpMethod;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
