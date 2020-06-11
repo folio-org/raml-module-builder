fd310dd6aeecc085b3b9cab753d667e0f7af3663
Julian Ladisch <julianladisch@users.noreply.github.com>
2020-06-11 12:39:21 +0200

    RMB-626: Remove sample and sample2 subprojects
    They are outdated.
    
    They are not referenced, I've checked
    * README.md
    * https://dev.folio.org
    
    All RMB integration tests should go into existing subproject
    https://github.com/folio-org/raml-module-builder/tree/master/domain-models-runtime-it
    
    [README.md](https://github.com/folio-org/raml-module-builder#build-and-run)
    lists these "sample projects":
    * https://github.com/folio-org/mod-configuration
    * https://github.com/folio-org/mod-notes

--- sample2/okapi-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -d@ModuleDescriptor.json http://localhost:9130/_/proxy/modules
-curl -d'{"id":"testlib"}' http://localhost:9130/_/proxy/tenants
-curl -d'[{"id":"mod-sample2-1.0.0", "action":"enable"}]' "http://localhost:9130/_/proxy/tenants/testlib/install?deploy=true"
