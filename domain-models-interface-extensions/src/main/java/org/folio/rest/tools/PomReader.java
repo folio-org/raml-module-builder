ded757a6e521a1fe8aee76c5cd3b2922186fae5f
Adam Dickmeiss <adam@indexdata.dk>
2020-08-21 12:57:42 +0200

    Use JarFile to sweep through Jar RMB-699 (#749)
    Add PomReaderTest

--- domain-models-interface-extensions/src/main/java/org/folio/rest/tools/PomReader.java
+++ domain-models-interface-extensions/src/main/java/org/folio/rest/tools/PomReader.java
@@ -1,19 +1,23 @@
 package org.folio.rest.tools;
 
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
-import java.util.Set;
-
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.folio.rest.tools.utils.ClassPath;
-
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
-
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 /**
  * @author shale
@@ -31,84 +35,107 @@ public enum PomReader {
 
   private final Logger log = LoggerFactory.getLogger(PomReader.class);
 
-  @SuppressWarnings("checkstyle:methodlength")
   private PomReader() {
+    init("pom.xml");
+  }
+
+  /**
+   * Read from pomFile if this is RMB itself; otherwise read JAR
+   * @param pomFilename
+   */
+  void init(String pomFilename) {
     try {
-      System.out.print("Attempting to read in the module name from....");
-      MavenXpp3Reader mavenreader = new MavenXpp3Reader();
-      Model model = null;
       String currentRunningJar =
           PomReader.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
-      System.out.println(currentRunningJar);
-      if(currentRunningJar != null && (currentRunningJar.contains("domain-models-runtime")
-          || currentRunningJar.contains("domain-models-interface-extensions"))){
-        //the runtime is the jar run when deploying during unit tests
-        //the interface-extensions is the jar run when running build time tools,
-        //like MDGenerator, ClientGenerator, etc..
-        //this is build time - not runtime, so just use the pom
-        File pomFile = new File("pom.xml");
-        model = mavenreader.read(new FileReader(pomFile));
-      }
-      else{
-        //this is runtime, the jar called via java -jar is the module's jar
-        ClassPath classPath = ClassPath.from(Thread.currentThread().getContextClassLoader());
-        Set<ClassPath.ResourceInfo> resources = classPath.getResources();
-        for (ClassPath.ResourceInfo info : resources) {
-          if(info.getResourceName().endsWith("pom.xml")){
-            //maven sets the classpath order according to the pom, so the poms project will be the first entry
-            model = mavenreader.read(PomReader.class.getResourceAsStream("/"+info.getResourceName()));
-            break;
-          }
-        }
-      }
-      if(model.getParent() != null){
-        moduleName = model.getParent().getArtifactId();
-        version = model.getParent().getVersion();
-      }
-      else{
-        moduleName = model.getArtifactId();
-      }
-
-      if(version == null){
-        version = model.getVersion();
-        if(version == null){
-          version = "1.0.0";
-        }
-      }
-      version = version.replaceAll("-.*", "");
-
-      moduleName = moduleName.replaceAll("-", "_");
-      props = model.getProperties();
-      dependencies = model.getDependencies();
-
-      //the version is a placeholder to a value in the props section
-      version = replacePlaceHolderWithValue(version);
-
-      for (int i = 0; i < dependencies.size(); i++) {
-        if("domain-models-runtime".equals(dependencies.get(i).getArtifactId())){
-          rmbVersion = dependencies.get(i).getVersion();
-          rmbVersion = replacePlaceHolderWithValue(rmbVersion);
-          rmbVersion = rmbVersion.replaceAll("-.*", "");
-        }
-      }
-
-      if(rmbVersion == null){
-        //if we are in the rmb jar - build time
-        rmbVersion = version;
-      }
-
-      //props.list(System.out);
-      log.info("module name: " + moduleName + ", version: " + version);
+      boolean readCurrent = currentRunningJar != null && (currentRunningJar.contains("domain-models-runtime")
+          || currentRunningJar.contains("domain-models-interface-extensions"));
+      readIt(readCurrent ? pomFilename : null, "META-INF/maven");
     } catch (Exception e) {
       log.error(e.getMessage(), e);
+      throw new IllegalArgumentException(e);
     }
+  }
+
+  /**
+   * Read from pomFile if not null; otherwise read JAR
+   * @param pomFilename POM filename; null for search in JAR
+   * @param directoryName directory prefix for search of pom.xml in JAR
+   */
+  void readIt(String pomFilename, String directoryName) throws IOException, XmlPullParserException {
+    Model model;
+    if (pomFilename != null) {
+      log.info("Reading from " + pomFilename);
+      //the runtime is the jar run when deploying during unit tests
+      //the interface-extensions is the jar run when running build time tools,
+      //like MDGenerator, ClientGenerator, etc..
+      //this is build time - not runtime, so just use the pom
+      File pomFile = new File(pomFilename);
+      MavenXpp3Reader mavenreader = new MavenXpp3Reader();
+      model = mavenreader.read(new FileReader(pomFile));
+    } else { //this is runtime, the jar called via java -jar is the module's jar
+      log.info("Reading from jar");
+      model = getModelFromJar(directoryName);
+    }
+    if (model.getParent() != null) {
+      moduleName = model.getParent().getArtifactId();
+      version = model.getParent().getVersion();
+    } else {
+      moduleName = model.getArtifactId();
+      version = model.getVersion();
+    }
+    version = version.replaceAll("-.*", "");
+
+    moduleName = moduleName.replaceAll("-", "_");
+    props = model.getProperties();
+    dependencies = model.getDependencies();
+
+    //the version is a placeholder to a value in the props section
+    version = replacePlaceHolderWithValue(version);
+
+    rmbVersion = null;
+    for (int i = 0; i < dependencies.size(); i++) {
+      if ("domain-models-runtime".equals(dependencies.get(i).getArtifactId())) {
+        rmbVersion = dependencies.get(i).getVersion();
+        rmbVersion = replacePlaceHolderWithValue(rmbVersion);
+        rmbVersion = rmbVersion.replaceAll("-.*", "");
+      }
+    }
+    if (rmbVersion == null) {
+      //if we are in the rmb jar - build time
+      rmbVersion = version;
+    }
+    log.info("module name: " + moduleName + ", version: " + version);
+  }
+
+  private Model getModelFromJar(String directoryName) throws IOException, XmlPullParserException {
+    MavenXpp3Reader mavenreader = new MavenXpp3Reader();
+    Model model = null;
+    URL url = Thread.currentThread().getContextClassLoader().getResource(directoryName);
+    if (url.getProtocol().equals("jar")) {
+      String dirname = directoryName + "/";
+      String path = url.getPath();
+      String jarPath = path.substring(5, path.indexOf('!'));
+      JarFile jar = new JarFile(URLDecoder.decode(jarPath, StandardCharsets.UTF_8.name()));
+      Enumeration<JarEntry> entries = jar.entries();
+      while (entries.hasMoreElements()) {
+        JarEntry entry = entries.nextElement();
+        String name = entry.getName();
+        // first pom.xml should be the right one.
+        if (name.startsWith(dirname) && !dirname.equals(name) && name.endsWith("pom.xml")) {
+          InputStream pomFile = PomReader.class.getClassLoader().getResourceAsStream(name);
+          model = mavenreader.read(pomFile);
+          break;
+        }
+      }
+    }
+    return model;
   }
 
   private String replacePlaceHolderWithValue(String placeholder){
     String ret[] = new String[]{placeholder};
-    if(placeholder != null && placeholder.startsWith("${")){
+    if (placeholder != null && placeholder.startsWith("${")) {
       props.forEach( (k,v) -> {
-        if(("${"+k+"}").equals(placeholder)){
+        if (("${"+k+"}").equals(placeholder)) {
           ret[0] = (String)v;
         }
       });
