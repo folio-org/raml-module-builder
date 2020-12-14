55fea65ae8c00cc890eb8ce067b22728cac5ba83
Adam Dickmeiss <adam@indexdata.dk>
2020-12-14 17:23:54 +0100

    Unused import

--- dbschema/src/main/java/org/folio/dbschema/ObjectMapperTool.java
+++ dbschema/src/main/java/org/folio/dbschema/ObjectMapperTool.java
@@ -1,6 +1,5 @@
 package org.folio.dbschema;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
