1a677f57f29bab866978970c0497d54809d4d883
Adam Dickmeiss <adam@indexdata.dk>
2021-02-03 16:49:37 +0100

    Document PostgresTester interface

--- util/src/main/java/org/folio/util/PostgresTester.java
+++ util/src/main/java/org/folio/util/PostgresTester.java
@@ -3,14 +3,35 @@ package org.folio.util;
 import java.io.Closeable;
 
 public interface PostgresTester extends Closeable {
+  /**
+   * start tester.
+   * @param database Postgres database
+   * @param username Postgres username
+   * @param password Postgres password
+   */
   void start(String database, String username, String password);
 
+  /**
+   * return listening port for spawned tester.
+   * @return port
+   */
   Integer getPort();
 
+  /**
+   * return host for tester.
+   * @return host
+   */
   String getHost();
 
+  /**
+   * has tester started.
+   * @return true if start has been invoked; false otherwise
+   */
   boolean isStarted();
 
+  /**
+   * close tester.
+   */
   @Override
   void close();
 }
