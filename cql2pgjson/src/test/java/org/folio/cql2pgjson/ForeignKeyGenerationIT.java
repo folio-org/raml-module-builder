78c199c9bbb11c0bb2e58587b1bc82e0a6737745
Julian Ladisch <julianladisch@users.noreply.github.com>
2021-04-20 15:25:21 +0200

    Rename tablej to tabled, use 'copyrightTracking'->>'copyrightStatusId'

--- cql2pgjson/src/test/java/org/folio/cql2pgjson/ForeignKeyGenerationIT.java
+++ cql2pgjson/src/test/java/org/folio/cql2pgjson/ForeignKeyGenerationIT.java
@@ -1,7 +1,7 @@
 package org.folio.cql2pgjson;
 
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.List;
 
--- cql2pgjson/src/test/resources/foreignKey.sql
+++ cql2pgjson/src/test/resources/foreignKey.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS tablea, tableb, tablec, tablef, tablej;
+DROP TABLE IF EXISTS tablea, tableb, tablec, tabled, tablef;
 CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA public;
 CREATE OR REPLACE FUNCTION f_unaccent(text) RETURNS text AS $$
   SELECT public.unaccent('public.unaccent', $1);
 $$ LANGUAGE sql IMMUTABLE;
+CREATE TABLE tabled (id UUID PRIMARY KEY, jsonb JSONB NOT NULL);
 CREATE TABLE tablef (id UUID PRIMARY KEY, jsonb JSONB NOT NULL);
-CREATE TABLE tablej (id UUID PRIMARY KEY, jsonb JSONB NOT NULL);
 CREATE TABLE tablea (id UUID PRIMARY KEY, jsonb JSONB NOT NULL);
-CREATE TABLE tableb (id UUID PRIMARY KEY, jsonb JSONB NOT NULL, tableaId UUID references tablea, tablefId UUID references tablef, tablejId UUID references tablej, copyrighttracking_copyrightstatusid UUID references tablej);
+CREATE TABLE tableb (id UUID PRIMARY KEY, jsonb JSONB NOT NULL, tableaId UUID references tablea, tablefId UUID references tablef, copyrighttracking_copyrightstatusid UUID references tabled);
 CREATE TABLE tablec (id UUID PRIMARY KEY, jsonb JSONB NOT NULL, tablebId UUID references tableb);
 
 CREATE OR REPLACE FUNCTION update_id() RETURNS TRIGGER AS $$
@@ -16,9 +16,9 @@ BEGIN
 END; $$ language 'plpgsql';
 CREATE TRIGGER update_tablea_references BEFORE INSERT OR UPDATE ON tablea
   FOR EACH ROW EXECUTE PROCEDURE update_id();
-CREATE TRIGGER update_tablef_references BEFORE INSERT OR UPDATE ON tablef
+CREATE TRIGGER update_tabled_references BEFORE INSERT OR UPDATE ON tabled
   FOR EACH ROW EXECUTE PROCEDURE update_id();
-CREATE TRIGGER update_tablej_references BEFORE INSERT OR UPDATE ON tablej
+CREATE TRIGGER update_tablef_references BEFORE INSERT OR UPDATE ON tablef
   FOR EACH ROW EXECUTE PROCEDURE update_id();
 
 CREATE OR REPLACE FUNCTION update_tableb_references() RETURNS TRIGGER AS $$
@@ -26,7 +26,7 @@ BEGIN
   NEW.id       = NEW.jsonb->>'id';
   NEW.tableaId = NEW.jsonb->>'tableaId';
   NEW.tablefId = NEW.jsonb->>'tablefId';
-  NEW.copyrightTracking_copyrightStatusId = NEW.jsonb->>'copyrightTracking_copyrightStatusId';
+  NEW.copyrightTracking_copyrightStatusId = NEW.jsonb->'copyrightTracking'->>'copyrightStatusId';
   RETURN NEW;
 END; $$ language 'plpgsql';
 CREATE TRIGGER update_tableb_references BEFORE INSERT OR UPDATE ON tableb
@@ -47,10 +47,9 @@ CREATE TRIGGER update_tablec_references BEFORE INSERT OR UPDATE ON tablec
 INSERT INTO tablef (jsonb) VALUES
 ('{"id": "F1111111-1111-4000-8000-000000000000"}');
 
-
-INSERT INTO tablej (jsonb) VALUES
-('{"id": "A1111111-1111-1111-1111-111111111111", "name": "cc1"}'),
-('{"id": "A2222222-2222-2222-2222-222222222222", "name": "cc2"}');
+INSERT INTO tabled (jsonb) VALUES
+('{"id": "D1111111-1111-1111-1111-111111111111", "name": "cc1"}'),
+('{"id": "D2222222-2222-2222-2222-222222222222", "name": "cc2"}');
 
 INSERT INTO tablea (jsonb) VALUES
 ('{"id": "A0000000-0000-0000-0000-000000000000", "name": "test0"}'),
@@ -62,13 +61,12 @@ INSERT INTO tablea (jsonb) VALUES
 INSERT INTO tablea (jsonb) VALUES (jsonb_build_object('id', md5('a' || generate_series(1, 2000)::text)));
 
 INSERT INTO tableb (jsonb) VALUES
-('{"id": "B1111111-1111-1111-1111-111111111111", "prefix": "x1", "otherindex": "y1","tableaId": "A1111111-1111-1111-1111-111111111111", "copyrightTracking_copyrightStatusId" : "a1111111-1111-1111-1111-111111111111"}'),
-('{"id": "B2222222-2222-2222-2222-222222222222", "prefix": "x2", "otherindex": "y2","tableaId": "A2222222-2222-2222-2222-222222222222", "copyrightTracking_copyrightStatusId" : "A2222222-2222-2222-2222-222222222222"}'),
-('{"id": "B3333333-3333-3333-3333-333333333333", "prefix": "x2", "otherindex": "y3","tableaId": "A2222222-2222-2222-2222-222222222222", "copyrightTracking_copyrightStatusId" : "A2222222-2222-2222-2222-222222222222"}'),
-('{"id": "B4444444-4444-4444-4444-444444444444", "prefix": "x0'')));(((''DROP tableb","otherindex": "y4", "tableaId": "A3333333-3333-3333-3333-333333333333", "copyrightTracking_copyrightStatusId" : "A2222222-2222-2222-2222-222222222222"}'),
-('{"id": "B5555555-5555-4000-8000-000000000000", "prefix": "x5", "tableaId": "A3333333-3333-3333-3333-333333333333", "copyrightTracking_copyrightStatusId" : "A2222222-2222-2222-2222-222222222222"}'),
-('{"id": "B6666666-6666-4000-8000-000000000000", "prefix": "x6", "tableaId": "A4444444-4444-4444-4444-444444444444", "tablefId": "F1111111-1111-4000-8000-000000000000", "copyrightTracking_copyrightStatusId" : "A2222222-2222-2222-2222-222222222222"}'),
-('{"id": "B7777777-7777-4000-8000-000000000000", "prefix": "x7", "tablejId": "A1111111-1111-1111-1111-111111111111", "copyrightTracking_copyrightStatusId" : "A2222222-2222-2222-2222-222222222222"}');
+('{"id": "B1111111-1111-1111-1111-111111111111", "prefix": "x1", "otherindex": "y1","tableaId": "A1111111-1111-1111-1111-111111111111", "copyrightTracking": {"copyrightStatusId": "D1111111-1111-1111-1111-111111111111"}}'),
+('{"id": "B2222222-2222-2222-2222-222222222222", "prefix": "x2", "otherindex": "y2","tableaId": "A2222222-2222-2222-2222-222222222222", "copyrightTracking": {"copyrightStatusId": "D2222222-2222-2222-2222-222222222222"}}'),
+('{"id": "B3333333-3333-3333-3333-333333333333", "prefix": "x2", "otherindex": "y3","tableaId": "A2222222-2222-2222-2222-222222222222"}'),
+('{"id": "B4444444-4444-4444-4444-444444444444", "prefix": "x0'')));(((''DROP tableb","otherindex": "y4", "tableaId": "A3333333-3333-3333-3333-333333333333"}'),
+('{"id": "B5555555-5555-4000-8000-000000000000", "prefix": "x5", "tableaId": "A3333333-3333-3333-3333-333333333333"}'),
+('{"id": "B6666666-6666-4000-8000-000000000000", "prefix": "x6", "tableaId": "A4444444-4444-4444-4444-444444444444", "tablefId": "F1111111-1111-4000-8000-000000000000"}');
 INSERT INTO tableb (jsonb) VALUES (jsonb_build_object('id', md5('b' || generate_series(1, 2000)::text)));
 
 INSERT INTO tablec (jsonb) VALUES
