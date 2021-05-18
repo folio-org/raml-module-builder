eea6ac6b5ab173a67fd645e2e12026411367e36f
Julian Ladisch <julianladisch@users.noreply.github.com>
2021-05-19 00:24:35 +0200

    Add test coverage

--- util/src/test/java/org/folio/util/PercentCodecTest.java
+++ util/src/test/java/org/folio/util/PercentCodecTest.java
@@ -2,7 +2,10 @@ package org.folio.util;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import org.folio.rest.testing.UtilityClassTester;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -24,10 +27,30 @@ class PercentCodecTest {
     "ääääääääää    , %C3%A4%C3%A4%C3%A4%C3%A4%C3%A4%C3%A4%C3%A4%C3%A4%C3%A4%C3%A4",
     "a b c         , a%20b%20c",
   })
-  void uriEncode(String s, String expected) {
-    assertThat(PercentCodec.encode(s).toString(), is(expected));
+  void encode(String s, String expected) {
+    assertThat(PercentCodec.encodeAsString(s), is(expected));
     StringBuffer stringBuffer = new StringBuffer("foo = ");
     PercentCodec.encode(stringBuffer, s);
     assertThat(stringBuffer.toString(), is("foo = " + expected));
+  }
+
+  @Test
+  void encodeThrowsUncheckedIOException() throws IOException {
+    var out = new Appendable() {
+      @Override
+      public Appendable append(CharSequence csq) throws IOException {
+        throw new IOException("failing mock");
+      }
+      @Override
+      public Appendable append(CharSequence csq, int start, int end) throws IOException {
+        throw new IOException("failing mock");
+      }
+      @Override
+      public Appendable append(char c) throws IOException {
+        throw new IOException("failing mock");
+      }
+    };
+
+    assertThrows(UncheckedIOException.class, () -> PercentCodec.encode(out, "foo"));
   }
 }
