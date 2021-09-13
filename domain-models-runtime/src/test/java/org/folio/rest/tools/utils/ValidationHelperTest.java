34827cb8d1ce719e471631c97c5b8e83e7bf536b
SinghAdes <Adesh_Singh@epam.com>
2021-09-13 11:19:03 +0530

    RMB-403 fixed

--- domain-models-runtime/src/test/java/org/folio/rest/tools/utils/ValidationHelperTest.java
+++ domain-models-runtime/src/test/java/org/folio/rest/tools/utils/ValidationHelperTest.java
@@ -3,7 +3,6 @@ package org.folio.rest.tools.utils;
 import org.folio.cql2pgjson.exception.QueryValidationException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import io.vertx.pgclient.PgException;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -11,11 +10,9 @@ import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 /**
  * @author shale
- *
  */
 @RunWith(VertxUnitRunner.class)
 public class ValidationHelperTest {
-
   private static final String QueryValidationException1 = "org.z3950.zing.cql.cql2pgjson.QueryValidationException: Field name 'abc' is not present in index.";
   private static final String QueryValidationException2 = "org.z3950.zing.cql.cql2pgjson.QueryValidationException: cql.serverChoice requested, but no serverChoiceIndexes defined.";
   private static final String QueryValidationException3 = "org.z3950.zing.cql.CQLParseException: expected index or term, got EOF";
@@ -24,7 +21,6 @@ public class ValidationHelperTest {
   public void uuidTest(TestContext context) {
     Async async = context.async();
     Throwable t = new PgException("invalid input syntax for uuid: \"1234567\"", null, "22P02", null);
-
     ValidationHelper.handleError(t, r -> {
       context.assertEquals(422, r.result().getStatus());
       async.complete();
@@ -35,15 +31,12 @@ public class ValidationHelperTest {
   public void dupTest(TestContext context) {
     Async async = context.async();
     Throwable t = new PgException("duplicate value violates unique constraint \"123456\"", null, "23505",
-      "Key (_id)=(55835c7c-2885-44f4-96ac-f03525b8e608) already exists.");
-
+        "Key (_id)=(55835c7c-2885-44f4-96ac-f03525b8e608) already exists.");
     ValidationHelper.handleError(t, r -> {
       context.assertEquals(422, r.result().getStatus());
-
       async.complete();
     });
   }
-
 
   @Test
   public void fkTest(TestContext context) {
@@ -52,7 +45,7 @@ public class ValidationHelperTest {
         "insert or update on table \"item\" violates foreign key constraint \"item_permanentloantypeid_fkey\"",
         null,
         "23503",
-    "Key (permanentloantypeid)=(2b94c631-fca9-4892-a730-03ee529ffe27) is not present in table \"loan_type\".");
+        "Key (permanentloantypeid)=(2b94c631-fca9-4892-a730-03ee529ffe27) is not present in table \"loan_type\".");
     ValidationHelper.handleError(t, r -> {
       context.assertEquals(422, r.result().getStatus());
       async.complete();
