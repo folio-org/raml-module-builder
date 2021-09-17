13b1e4a5a5c73a671a9904a07d849d5c1a62f788
SinghAdes <Adesh_Singh@epam.com>
2021-09-17 12:42:15 +0530

    RMB-850 bug fix

--- domain-models-runtime/src/test/java/org/folio/rest/security/AESTest.java
+++ domain-models-runtime/src/test/java/org/folio/rest/security/AESTest.java
@@ -3,9 +3,10 @@ package org.folio.rest.security;
 import org.folio.rest.testing.UtilityClassTester;
 import org.junit.Test;
 
+import javax.crypto.SecretKey;
+
 public final class AESTest {
   @Test
   public void UtilityClass() {
-    UtilityClassTester.assertUtilityClass(AES.class);
-  }
+    UtilityClassTester.assertUtilityClass(AES.class);}
 }
