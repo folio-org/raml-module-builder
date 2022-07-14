46d1c466205545a107dc392a6831ff49534374ee
Adam Dickmeiss <adam@indexdata.dk>
2022-07-14 18:26:01 +0200

    Simplify OutStreamTest

--- domain-models-runtime/src/test/java/org/folio/rest/tools/utils/OutStreamTest.java
+++ domain-models-runtime/src/test/java/org/folio/rest/tools/utils/OutStreamTest.java
@@ -4,53 +4,11 @@ import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-
-import org.junit.After;
-import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class OutStreamTest {
-  private Charset oldDefaultCharset;
-
-  private static int major;
-
-  @BeforeClass
-  public static void beforeClass() {
-    String version = System.getProperty("java.version");
-    int dot = version.indexOf('.');
-    major = Integer.parseInt(version.substring(0, dot));
-  }
-
-  private void setDefaultCharset(Charset charset) {
-    Assume.assumeTrue(major < 17);
-    if (oldDefaultCharset == null) {
-      oldDefaultCharset = Charset.defaultCharset();
-    }
-    try {
-      // default charset is cached by Charset, use reflection to change it
-      Class<Charset> clazz = Charset.class;
-      Field field = clazz.getDeclaredField("defaultCharset");
-      field.setAccessible(true);
-      field.set(null, charset);
-    }
-    catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  @After
-  public void tearDown() {
-    if (oldDefaultCharset == null) {
-      return;
-    }
-    // defaultCharset has been changed, restore it
-    setDefaultCharset(oldDefaultCharset);
-  }
-
   @Test
   public void getWithoutSet() {
     assertNull(new OutStream().getData());
@@ -84,39 +42,14 @@ public class OutStreamTest {
     new OutStream().write(new ByteArrayOutputStream());
   }
 
-  private void testBytes() {
-    try {
-      OutStream s = new OutStream();
-      s.setData("ä");
-      ByteArrayOutputStream result = new ByteArrayOutputStream();
-      s.write(result);
-      assertArrayEquals("ä".getBytes(StandardCharsets.UTF_8), result.toByteArray());
-    }
-    catch (Exception e) {
-      throw new RuntimeException(e);
-    }
+  @Test
+  public void utf8() throws IOException {
+    OutStream s = new OutStream();
+    s.setData("ä");
+    assertEquals("ä", s.getData());
+    ByteArrayOutputStream result = new ByteArrayOutputStream();
+    s.write(result);
+    assertArrayEquals("ä".getBytes(StandardCharsets.UTF_8), result.toByteArray());
   }
 
-  @Test
-  public void defaultCharset() {
-    testBytes();
-  }
-
-  @Test
-  public void utf8() {
-    setDefaultCharset(StandardCharsets.UTF_8);
-    testBytes();
-  }
-
-  @Test
-  public void usAscii() {
-    setDefaultCharset(StandardCharsets.US_ASCII);
-    testBytes();
-  }
-
-  @Test
-  public void iso_8859_1() throws IOException {
-    setDefaultCharset(StandardCharsets.ISO_8859_1);
-    testBytes();
-  }
 }
