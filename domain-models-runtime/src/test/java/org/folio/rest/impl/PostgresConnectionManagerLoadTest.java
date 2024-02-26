894252af60719e10c796617e5641824e027738a3
Olamide Kolawole <okolawole@ebsco.com>
2024-02-26 10:03:03 -0600

    load test random tenant with significant duration for each DB call

--- domain-models-runtime/src/test/java/org/folio/rest/impl/PostgresConnectionManagerLoadTest.java
+++ domain-models-runtime/src/test/java/org/folio/rest/impl/PostgresConnectionManagerLoadTest.java
@@ -1,19 +1,35 @@
 package org.folio.rest.impl;
 
+import io.vertx.core.Future;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.Timeout;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.folio.postgres.testing.PostgresTesterContainer;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.persist.PostgresClientHelper;
 import org.folio.rest.tools.utils.VertxUtils;
-import org.junit.*;
-import org.junit.rules.Timeout;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
 
 @RunWith(VertxUnitRunner.class)
 public class PostgresConnectionManagerLoadTest extends TenantHelper {
+  private final Logger LOG = LogManager.getLogger(PostgresConnectionManagerLoadTest.class);
+
   @Rule
-  public Timeout rule = Timeout.seconds(20);
+  public Timeout rule = Timeout.seconds(30);
 
   @BeforeClass
   public static void setUpClass() {
@@ -39,6 +55,69 @@ public class PostgresConnectionManagerLoadTest extends TenantHelper {
     putLoadOnTenant("tenant4", context);
     // Add more tenants to increase demand on cache.
   }
+
+
+  @Test
+  public void randomTenantsWithLongDBCallDuration(TestContext context) {
+    Async async = context.async();
+    int numTenants = 15;
+    int numAssertions = 50;
+    Random rand = new Random();
+    Function<Integer, String> tenantNameFunc = (index) -> "tenant" + index;
+
+    PostgresClientHelper.setSharedPgPool(true);
+    Future.succeededFuture()
+        .compose(ar -> {
+          // Setup
+          Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.ERROR);
+
+          for (int i = 0; i < numTenants; i++) {
+            tenantPost(new TenantAPI(), context, null, tenantNameFunc.apply(i));
+          }
+          Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.DEBUG);
+          return Future.succeededFuture();
+        })
+        .compose(ar -> {
+          // Load testing
+          List<Future<Void>> futures = new ArrayList<>();
+          for (int i = 0; i < numAssertions; i++) {
+            int tenantIndex = rand.nextInt(numTenants);
+            futures.add(exerciseTenant(tenantNameFunc.apply(tenantIndex), context));
+          }
+          return Future.all(futures);
+        })
+        .compose(notUsed -> {
+          // Clean up
+          Future<Void> future = Future.succeededFuture();
+          Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.ERROR);
+          for (int i = 0; i < numTenants; i++) {
+            String tenantName = tenantNameFunc.apply(i);
+            future = future.compose(ar -> purgeTenant(tenantName).mapEmpty());
+          }
+          return future;
+        }).onComplete(ar -> {
+          if(ar.failed()){
+            LOG.error(ar.cause());
+            context.fail(ar.cause());
+          }
+          Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.DEBUG);
+          PostgresClient.closeAllClients();
+          async.complete();
+        });
+  }
+
+  private Future<Void> exerciseTenant(String tenant, TestContext context) {
+    PostgresClient postgresClient = PostgresClient.getInstance(vertx, tenant);
+    return postgresClient.selectSingle("select current_user; select pg_sleep(0.5);")
+        .onFailure(th -> LOG.error("Something happened while querying database for tenant:{}",tenant, th))
+        .compose(ar -> {
+          LOG.info("got result for {}", tenant);
+          context.assertEquals(tenant + "_raml_module_builder", ar.getString(0));
+          return Future.succeededFuture();
+        });
+  }
+
+  ;
 
   private void putLoadOnTenant(String tenant, TestContext context) {
     PostgresClient.getInstance(vertx, tenant).execute("INSERT INTO test_tenantapi VALUES ('27f0857b-3165-4d5a-af77-229e4ad7921d', '{}')")
