86a4e6543ffbf947872c0ac8c1add29171e6cd40
Julian Ladisch <julianladisch@users.noreply.github.com>
2024-04-04 19:44:01 +0200

    Remove unused import

--- dbschema/src/main/java/org/folio/dbschema/ObjectMapperTool.java
+++ dbschema/src/main/java/org/folio/dbschema/ObjectMapperTool.java
@@ -1,6 +1,5 @@
 package org.folio.dbschema;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
