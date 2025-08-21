070a6f7a435e2962ee764fb066541989c22fe70d
Adam Dickmeiss <adam@indexdata.dk>
2025-08-21 10:02:57 +0200

    Begin Vert.x 5 migration

--- domain-models-runtime/src/main/java/org/folio/rest/RestLauncher.java
+++ domain-models-runtime/src/main/java/org/folio/rest/RestLauncher.java
@@ -1,40 +1,35 @@
 package org.folio.rest;
 
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Launcher;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxBuilder;
 import io.vertx.core.VertxOptions;
+import io.vertx.launcher.application.HookContext;
+import io.vertx.launcher.application.VertxApplication;
+import io.vertx.launcher.application.VertxApplicationHooks;
 import org.folio.okapi.common.MetricsUtil;
 
-public class RestLauncher extends Launcher {
+public class RestLauncher extends VertxApplication {
 
   public static void main(String[] args) {
-    new RestLauncher().dispatch(args);
+    System.setProperty("vertx.logger-delegate-factory-class-name",
+        "io.vertx.core.logging.Log4jLogDelegateFactory");
+    VertxApplicationHooks hooks = new VertxApplicationHooks() {
+      @Override
+      public void beforeStartingVertx(HookContext context) {
+        System.out.println("starting rest verticle service..........");
+        VertxOptions options = context.vertxOptions();
+        options.setBlockedThreadCheckInterval(1500000);
+        options.setWarningExceptionTime(1500000);
+        VertxBuilder vb = Vertx.builder();
+        MetricsUtil.init(vb);
+      }
+    };
+
+    VertxApplication vertxApplication = new RestLauncher(args, hooks);
+    vertxApplication.launch();
   }
 
-  @Override
-  public void beforeStartingVertx(VertxOptions options) {
-    super.beforeStartingVertx(options);
-
-    System.out.println("starting rest verticle service..........");
-    options.setBlockedThreadCheckInterval(1500000);
-    options.setWarningExceptionTime(1500000);
-    MetricsUtil.init(options);
+  public RestLauncher(String[] args, VertxApplicationHooks hooks) {
+    super(args, hooks);
   }
-
-  @Override
-  public void afterStartingVertx(Vertx vertx) {
-    super.afterStartingVertx(vertx);
-  }
-
-  @Override
-  public void beforeDeployingVerticle(DeploymentOptions deploymentOptions) {
-    super.beforeDeployingVerticle(deploymentOptions);
-  }
-
-  @Override
-  public void handleDeployFailed(Vertx vertx, String mainVerticle, DeploymentOptions deploymentOptions, Throwable cause) {
-    super.handleDeployFailed(vertx, mainVerticle, deploymentOptions, cause);
-  }
-
 }
