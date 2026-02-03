1fc84f83a44083560513525fb686962661be1064
Siarhei Charniak <siarhei_charniak@epam.com>
2026-02-03 10:46:11 +0300

    MODINVSTOR-1456 - Implement of PATCH requests for instances (storage layer)

--- domain-models-runtime/src/main/java/org/folio/rest/persist/PgUtil.java
+++ domain-models-runtime/src/main/java/org/folio/rest/persist/PgUtil.java
@@ -29,6 +29,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.dbschema.ObjectMapperTool;
+import org.folio.rest.persist.Criteria.Criteria;
+import org.folio.rest.persist.Criteria.Criterion;
+import org.folio.rest.persist.Criteria.UpdateSection;
 import org.folio.rest.tools.utils.MetadataUtil;
 import org.folio.rest.tools.utils.OptimisticLockingUtil;
 import org.folio.rest.tools.utils.OutStream;
@@ -1353,6 +1356,99 @@ public final class PgUtil {
           String message = "Updated " + updated + " records in " + table + " for id: " + id;
           logger.fatal(message);
           response(message, respond500, respond500).onComplete(promise);
+          return;
+        }
+        response(respond204, respond500).onComplete(promise);
+      });
+      return promise.future();
+    } catch (Exception e) {
+      logger.error(e.getMessage(), e);
+      return response(e.getMessage(), respond500, respond500);
+    }
+  }
+
+  /**
+   * Patch entity in the table.
+   *
+   * <p>To enforce optimistic locking the version of entity is set to null if it is -1.
+   *
+   * <p>All exceptions are caught and reported via the asyncResultHandler.
+   *
+   * @param table  table name
+   * @param updateSection  patch data
+   * @param id  the id value to use for entity
+   * @param okapiHeaders  http headers provided by okapi
+   * @param vertxContext  the current context
+   * @param clazz  the ResponseDelegate class created from the RAML file with these methods:
+   *               respond204(), respond400WithTextPlain(Object), respond404WithTextPlain(Object),
+   *               respond409WithTextPlain(Object), respond500WithTextPlain(Object).
+   * @param asyncResultHandler  where to return the result created by clazz
+   */
+  public static void patch(String table, UpdateSection updateSection, String id,
+      Map<String, String> okapiHeaders, Context vertxContext,
+      Class<? extends ResponseDelegate> clazz,
+      Handler<AsyncResult<Response>> asyncResultHandler) {
+    patch(table, updateSection, id, okapiHeaders, vertxContext, clazz)
+        .onComplete(asyncResultHandler);
+  }
+
+  /**
+   * Patch entity in the table.
+   *
+   * <p>To enforce optimistic locking the version of entity is set to null if it is -1.
+   *
+   * <p>All exceptions are caught and reported via the asyncResultHandler.
+   *
+   * @param table  table name
+   * @param updateSection  the new entity to store. The id field is set to the id value.
+   * @param id  the id value to use for entity
+   * @param okapiHeaders  http headers provided by okapi
+   * @param vertxContext  the current context
+   * @param clazz  the ResponseDelegate class created from the RAML file with these methods:
+   *               respond204(), respond400WithTextPlain(Object), respond404WithTextPlain(Object),
+   *               respond409WithTextPlain(Object), respond500WithTextPlain(Object).
+   * @return   where to return the result created by clazz
+   */
+  public static Future<Response> patch(String table, UpdateSection updateSection, String id,
+      Map<String, String> okapiHeaders, Context vertxContext,
+      Class<? extends ResponseDelegate> clazz) {
+
+    var criterion = new Criterion().addCriterion(new Criteria()
+        .addField("'id'").setOperation("=").setVal(id));
+
+    final Method respond500;
+
+    try {
+      respond500 = clazz.getMethod(RESPOND_500_WITH_TEXT_PLAIN, Object.class);
+    } catch (Exception e) {
+      logger.error(e.getMessage(), e);
+      return response(e.getMessage(), null, null);
+    }
+
+    try {
+      Method respond204 = clazz.getMethod(RESPOND_204);
+      Method respond400 = clazz.getMethod(RESPOND_400_WITH_TEXT_PLAIN, Object.class);
+      Method respond404 = clazz.getMethod(RESPOND_404_WITH_TEXT_PLAIN, Object.class);
+      Method respond409 = getRespond409(clazz);
+      if (! UuidUtil.isUuid(id)) {
+        return responseInvalidUuid(table + ".id", id, clazz, respond400, respond500);
+      }
+
+      final Promise<Response> promise = Promise.promise();
+      PostgresClient postgresClient = postgresClient(vertxContext, okapiHeaders);
+      postgresClient.update(table, updateSection, criterion, false, reply -> {
+        if (reply.failed()) {
+          if (PgExceptionUtil.isVersionConflict(reply.cause())) {
+            Method method = respond409 == null ? respond400 : respond409;
+            response(reply.cause().getMessage(), method, respond500).onComplete(promise);
+          } else {
+            response(table, id, reply.cause(), clazz, respond400, respond500).onComplete(promise);
+          }
+          return;
+        }
+        int updated = reply.result().rowCount();
+        if (updated == 0) {
+          response(NOT_FOUND, respond404, respond500).onComplete(promise);
           return;
         }
         response(respond204, respond500).onComplete(promise);
