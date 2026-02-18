f154f454fb00679bc723be2300e3ad8b12f6819e
Steve Ellis <sellis@ebsco.com>
2026-02-18 18:59:25 -0500

    Sonar fixes

--- domain-models-runtime/src/main/java/org/folio/rest/tools/utils/ConnectionCacheMetrics.java
+++ domain-models-runtime/src/main/java/org/folio/rest/tools/utils/ConnectionCacheMetrics.java
@@ -1,98 +1,102 @@
 package org.folio.rest.tools.utils;
 
 public class ConnectionCacheMetrics {
-    private int hits;
-    private int misses;
-    private int active;
-    private int poolSize;
-    private int recycled;
-    private int recycleErrors;
-    private int newConnections;
-    private int newConnectionErrors;
+  private int hits;
+  private int misses;
+  private int active;
+  private int poolSize;
+  private int recycled;
+  private int recycleErrors;
+  private int newConnections;
+  private int newConnectionErrors;
 
-    public int getHits() {
-        return hits;
-    }
+  public ConnectionCacheMetrics() {
+    // default constructor
+  }
 
-    public int getMisses() {
-        return misses;
-    }
+  public int getHits() {
+    return hits;
+  }
 
-    public int getActive() {
-        return active;
-    }
+  public int getMisses() {
+    return misses;
+  }
 
-    public int getPoolSize() {
-        return poolSize;
-    }
+  public int getActive() {
+    return active;
+  }
 
-    public void setPoolSize(int poolSize) {
-        this.poolSize = poolSize;
-    }
+  public int getPoolSize() {
+    return poolSize;
+  }
 
-    public int getRecycled() {
-        return recycled;
-    }
+  public void setPoolSize(int poolSize) {
+    this.poolSize = poolSize;
+  }
 
-    public int getRecycleErrors() {
-        return recycleErrors;
-    }
+  public int getRecycled() {
+    return recycled;
+  }
 
-    public int getNewConnections() {
-        return newConnections;
-    }
+  public int getRecycleErrors() {
+    return recycleErrors;
+  }
 
-    public int getNewConnectionErrors() {
-        return newConnectionErrors;
-    }
+  public int getNewConnections() {
+    return newConnections;
+  }
 
-    public void incrementActive() {
-        active++;
-    }
+  public int getNewConnectionErrors() {
+    return newConnectionErrors;
+  }
 
-    public void decrementActive() {
-        active--;
-    }
+  public void incrementActive() {
+    active++;
+  }
 
-    public void clear() {
-      hits = 0;
-      misses = 0;
-      active = 0;
-      recycled = 0;
-      recycleErrors = 0;
-      newConnections = 0;
-      newConnectionErrors = 0;
-    }
+  public void decrementActive() {
+    active--;
+  }
 
-    public void incrementHits() {
-      hits = (hits == Integer.MAX_VALUE) ? 0 : (hits + 1);
-    }
+  public void clear() {
+    hits = 0;
+    misses = 0;
+    active = 0;
+    recycled = 0;
+    recycleErrors = 0;
+    newConnections = 0;
+    newConnectionErrors = 0;
+  }
 
-    public void incrementMisses() {
-      misses = (misses == Integer.MAX_VALUE) ? 0 : (misses + 1);
-    }
+  public void incrementHits() {
+    hits = (hits == Integer.MAX_VALUE) ? 0 : (hits + 1);
+  }
 
-    public void incrementNewConnections() {
-      newConnections = (newConnections == Integer.MAX_VALUE) ? 0 : (newConnections + 1);
-    }
+  public void incrementMisses() {
+    misses = (misses == Integer.MAX_VALUE) ? 0 : (misses + 1);
+  }
 
-    public void incrementNewConnectionErrors() {
-      newConnectionErrors = (newConnectionErrors == Integer.MAX_VALUE) ? 0 : (newConnectionErrors + 1);
-    }
+  public void incrementNewConnections() {
+    newConnections = (newConnections == Integer.MAX_VALUE) ? 0 : (newConnections + 1);
+  }
 
-    public void incrementRecycled() {
-      recycled = (recycled == Integer.MAX_VALUE) ? 0 : (recycled + 1);
-    }
+  public void incrementNewConnectionErrors() {
+    newConnectionErrors = (newConnectionErrors == Integer.MAX_VALUE) ? 0 : (newConnectionErrors + 1);
+  }
 
-    public void incrementRecycleErrors() {
-      recycleErrors = (recycleErrors == Integer.MAX_VALUE) ? 0 : (recycleErrors + 1);
-    }
+  public void incrementRecycled() {
+    recycled = (recycled == Integer.MAX_VALUE) ? 0 : (recycled + 1);
+  }
 
-    public String toString(String msg, int cacheSize) {
-        return msg + String.format(
-                ":: %s hits, %s misses, %s recycled, %s recycleErrors, %s newConnections, "
-                + "%s newConnectionErrors, %s size, %s active, %s pool",
-                hits, misses, recycled, recycleErrors, newConnections, newConnectionErrors,
-                cacheSize, active, poolSize);
-    }
+  public void incrementRecycleErrors() {
+    recycleErrors = (recycleErrors == Integer.MAX_VALUE) ? 0 : (recycleErrors + 1);
+  }
+
+  public String toString(String msg, int cacheSize) {
+    return msg + String.format(
+        ":: %s hits, %s misses, %s recycled, %s recycleErrors, %s newConnections, "
+            + "%s newConnectionErrors, %s size, %s active, %s pool",
+        hits, misses, recycled, recycleErrors, newConnections, newConnectionErrors,
+        cacheSize, active, poolSize);
+  }
 }
