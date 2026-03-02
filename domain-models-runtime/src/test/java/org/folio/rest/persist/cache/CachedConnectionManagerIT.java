d7a6b7011b26ec7b02648864832b4987ba72f78b
Steve Ellis <sellis@ebsco.com>
2026-03-02 15:48:23 -0500

    Rewrite of CachedConnectionManagerIT for Junit 6 and compose

--- domain-models-runtime/src/test/java/org/folio/rest/persist/cache/CachedConnectionManagerIT.java
+++ domain-models-runtime/src/test/java/org/folio/rest/persist/cache/CachedConnectionManagerIT.java
@@ -1,7 +1,9 @@
 package org.folio.rest.persist.cache;
 
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 import io.vertx.pgclient.impl.PgConnectionImpl;
 import org.folio.postgres.testing.PostgresTesterContainer;
 import org.folio.rest.impl.TenantAPI;
@@ -9,143 +11,160 @@ import org.folio.rest.impl.TenantHelper;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.persist.PostgresClientHelper;
 import org.folio.rest.tools.utils.VertxUtils;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 
-@RunWith(VertxUnitRunner.class)
-public class CachedConnectionManagerIT extends TenantHelper {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
-  @BeforeClass
-  public static void setUpClass() {
+@ExtendWith(VertxExtension.class)
+class CachedConnectionManagerIT extends TenantHelper {
+
+  @BeforeAll
+  static void setUpClass() {
     vertx = VertxUtils.getVertxWithExceptionHandler();
     PostgresClient.setPostgresTester(new PostgresTesterContainer());
   }
 
-  @AfterClass
-  public static void afterClass() {
+  @AfterAll
+  static void afterClass() {
     PostgresClientHelper.setSharedPgPool(false);
   }
 
-  @Before
-  public void beforeEach() {
+  @BeforeEach
+  void beforeEachTest() {
     PostgresClientHelper.setSharedPgPool(true);
     PostgresClient.clearConnectionCache();
   }
 
   @Test
-  public void shouldNotCreateNestedWrappers(TestContext context) {
-    tenantPost(new TenantAPI(), context, null, "tenant1");
-    tenantPost(new TenantAPI(), context, null, "tenant2");
+  void shouldNotCreateNestedWrappers(VertxTestContext testContext) {
+    var originalSessionId = new AtomicReference<UUID>();
 
-    var async = context.async();
-    var pgClient1 = PostgresClient.getInstance(vertx, "tenant1");
-    pgClient1.getConnection().onComplete(context.asyncAssertSuccess(conn1 -> {
-      var originalSessionId = ((CachedPgConnection) conn1).getSessionId();
-      // Closing the connection returns it to the cache so it can be recycled.
-      conn1.close();
-
-      var pgClient2 = PostgresClient.getInstance(vertx, "tenant2");
-      pgClient2.getConnection().onComplete(context.asyncAssertSuccess(conn2 -> {
-        var recycledCached = (CachedPgConnection) conn2;
-
-        assertEquals(originalSessionId, recycledCached.getSessionId());
-        // Should still wrap the REAL connection, not the cached one
-        assertEquals(PgConnectionImpl.class, recycledCached.getWrappedConnection().getClass());
-        assertNotEquals(CachedPgConnection.class, recycledCached.getWrappedConnection().getClass());
-
-        conn2.close().onComplete(context.asyncAssertSuccess(v -> async.complete()));
-      }));
-    }));
+    prepareTenants("tenant1", "tenant2")
+        .compose(v -> getConnection("tenant1"))
+        .compose(conn1 -> {
+          originalSessionId.set(conn1.getSessionId());
+          return closeConnection(conn1); // Return it to the cache, so it can be reused as is for tenant2.
+        })
+        .compose(v -> getConnection("tenant2"))
+        .compose(conn2 -> {
+          assertThat(conn2.getSessionId(), is(originalSessionId.get()));
+          assertThat(conn2.getWrappedConnection().getClass(), is(PgConnectionImpl.class));
+          return conn2.close();
+        })
+        .onComplete(testContext.succeedingThenComplete());
   }
 
   @Test
-  public void shouldUpdateConnectionMetadataAfterConnectionSucceeds(TestContext context) {
-    tenantPost(new TenantAPI(), context, null, "tenant1");
-    tenantPost(new TenantAPI(), context, null, "tenant2");
+  void shouldUpdateConnectionMetadataAfterConnectionSucceeds(VertxTestContext testContext) {
+    var originalSessionId = new AtomicReference<UUID>();
+    var firstConnectionRef = new AtomicReference<CachedPgConnection>();
 
-    var async = context.async();
-    var pgClient1 = PostgresClient.getInstance(vertx, "tenant1");
-    pgClient1.getConnection().onComplete(context.asyncAssertSuccess(conn1 -> {
-      var recycledCached1 = (CachedPgConnection) conn1;
-      var originalSessionId = recycledCached1.getSessionId();
+    prepareTenants("tenant1", "tenant2")
+        .compose(v -> getConnection("tenant1"))
+        .compose(conn1 -> {
+          firstConnectionRef.set(conn1);
+          originalSessionId.set(conn1.getSessionId());
 
-      assertEquals("tenant1", recycledCached1.getTenantId());
-      assertEquals("tenant1_raml_module_builder", recycledCached1.getSchemaName());
+          assertThat(conn1.getTenantId(), is("tenant1"));
+          assertThat(conn1.getSchemaName(), is("tenant1_raml_module_builder"));
 
-      // Closing the connection returns it to the cache so it can be recycled.
-      conn1.close();
-
-      // But before it is recycled, it should still have the previous metadata.
-      assertEquals("tenant1", recycledCached1.getTenantId());
-      assertEquals("tenant1_raml_module_builder", recycledCached1.getSchemaName());
-
-      var pgClient2 = PostgresClient.getInstance(vertx, "tenant2");
-      pgClient2.getConnection().onComplete(context.asyncAssertSuccess(conn2 -> {
-        var recycledCached2 = (CachedPgConnection) conn2;
-
-        assertEquals(originalSessionId, recycledCached2.getSessionId());
-        assertEquals("tenant2", recycledCached2.getTenantId());
-        assertEquals("tenant2_raml_module_builder", recycledCached2.getSchemaName());
-
-        conn2.close().onComplete(context.asyncAssertSuccess(v -> async.complete()));
-      }));
-    }));
+          return closeConnection(conn1, () -> {
+            // Before recycling it should still have the original metadata.
+            assertThat(firstConnectionRef.get().getTenantId(), is("tenant1"));
+            assertThat(firstConnectionRef.get().getSchemaName(), is("tenant1_raml_module_builder"));
+          });
+        })
+        .compose(v -> getConnection("tenant2"))
+        .compose(conn2 -> {
+          assertThat(conn2.getSessionId(), is(originalSessionId.get()));
+          assertThat(conn2.getTenantId(), is("tenant2"));
+          assertThat(conn2.getSchemaName(), is("tenant2_raml_module_builder"));
+          return conn2.close();
+        })
+        .onComplete(testContext.succeedingThenComplete());
   }
 
   @Test
-  public void shouldRecoverOnSetRoleFailure(TestContext context) {
-    tenantPost(new TenantAPI(), context, null, "tenant1");
+  void shouldRecoverOnSetRoleFailure(VertxTestContext testContext) {
+    var originalSessionId = new AtomicReference<UUID>();
 
-    var async = context.async();
-    var pgClient1 = PostgresClient.getInstance(vertx, "tenant1");
-    pgClient1.getConnection().onComplete(context.asyncAssertSuccess(conn1 -> {
-      var cachedConn = (CachedPgConnection) conn1;
-      var originalSessionId = cachedConn.getSessionId();
-
-      // Closing the connection returns it to the cache so it can be recycled.
-      conn1.close();
-
-      // Try to get a connection for a tenant that does not exist. This will cause the SET ROLE to fail.
-      var pgClient2 = PostgresClient.getInstance(vertx, "nonexistent");
-      pgClient2.getConnection().onComplete(context.asyncAssertFailure(failure -> {
-        // After the failure, the connection should have been returned to the cache with its original tenant.
-        // Let's try to get it again for the original tenant.
-        pgClient1.getConnection().onComplete(context.asyncAssertSuccess(conn3 -> {
-          var cachedConn3 = (CachedPgConnection) conn3;
-
-          // The connection should be the same one we got before.
-          assertEquals(originalSessionId, cachedConn3.getSessionId());
-          assertEquals("tenant1", cachedConn3.getTenantId());
-          assertEquals("tenant1_raml_module_builder", cachedConn3.getSchemaName());
-
-          conn3.close().onComplete(context.asyncAssertSuccess(v -> async.complete()));
-        }));
-      }));
-    }));
+    prepareTenants("tenant1")
+        .compose(v -> getConnection("tenant1"))
+        .compose(conn1 -> {
+          originalSessionId.set(conn1.getSessionId());
+          return recoverAfterFailure("tenant1", "nonexistent", conn1);
+        })
+        .compose(conn3 -> {
+          assertThat(conn3.getSessionId(), is(originalSessionId.get()));
+          assertThat(conn3.getTenantId(), is("tenant1"));
+          assertThat(conn3.getSchemaName(), is("tenant1_raml_module_builder"));
+          return conn3.close();
+        })
+        .onComplete(testContext.succeedingThenComplete());
   }
 
   @Test
-  public void shouldRecoverOnSetRoleFailureForNewConnection(TestContext context) {
-    tenantPost(new TenantAPI(), context, null, "diku");
+  void shouldRecoverOnSetRoleFailureForNewConnection(VertxTestContext testContext) {
+    prepareTenants("diku")
+        .compose(v -> expectFailure(PostgresClient.getInstance(vertx, "nonexistent").getConnection()))
+        .compose(ignored -> getConnection("diku"))
+        .compose(conn -> {
+          assertThat(conn.getTenantId(), is("diku"));
+          return conn.close();
+        })
+        .onComplete(testContext.succeedingThenComplete());
+  }
 
-    var async = context.async();
-    // Try to get a connection for a tenant that does not exist. This will cause the SET ROLE to fail.
-    var pgClient1 = PostgresClient.getInstance(vertx, "nonexistent");
-    pgClient1.getConnection().onComplete(context.asyncAssertFailure(failure -> {
-      // After the failure, the underlying connection should have been closed and returned to the pool.
-      // Let's try to get a connection for a valid tenant to prove the pool is healthy.
-      var pgClient2 = PostgresClient.getInstance(vertx, "diku");
-      pgClient2.getConnection().onComplete(context.asyncAssertSuccess(conn -> {
-        // The connection should be a valid one.
-        assertEquals("diku", ((CachedPgConnection) conn).getTenantId());
-        conn.close().onComplete(context.asyncAssertSuccess(v -> async.complete()));
-      }));
-    }));
+  private Future<CachedPgConnection> getConnection(String tenant) {
+    return PostgresClient.getInstance(vertx, tenant)
+        .getConnection()
+        .map(conn -> (CachedPgConnection) conn);
+  }
+
+  private Future<CachedPgConnection> recoverAfterFailure(String originalTenant, String failingTenant, CachedPgConnection cachedConn) {
+    return closeConnection(cachedConn)
+        .compose(v -> expectFailure(PostgresClient.getInstance(vertx, failingTenant).getConnection()))
+        .compose(v -> getConnection(originalTenant));
+  }
+
+  private Future<Void> closeConnection(CachedPgConnection connection) {
+    return closeConnection(connection, null);
+  }
+
+  private Future<Void> closeConnection(CachedPgConnection connection, Runnable afterClose) {
+    return connection.close().onSuccess(v -> {
+      if (afterClose != null) {
+        afterClose.run();
+      }
+    });
+  }
+
+  private Future<Void> prepareTenants(String... tenants) {
+    var api = new TenantAPI();
+    Future<Void> chain = Future.succeededFuture();
+    for (String tenant : tenants) {
+      chain = chain.compose(v -> tenantPostSync(api, null, tenant));
+    }
+    return chain;
+  }
+
+  private static <T> Future<Void> expectFailure(Future<T> future) {
+    Promise<Void> promise = Promise.promise();
+    future.onComplete(ar -> {
+      if (ar.succeeded()) {
+        promise.fail("Expected future to fail, but it succeeded");
+      } else {
+        promise.complete();
+      }
+    });
+    return promise.future();
   }
 }
